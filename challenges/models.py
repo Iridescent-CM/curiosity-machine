c9472da341bb9984e395d0e93e69dced80339908
Andrew Gorcester <andrew.gorcester@gmail.com>
2014-03-21 17:42:55 -0700

    add challenges route; minimalist challenge page with no steps yet

--- challenges/models.py
+++ challenges/models.py
@@ -1,26 +1,7 @@
 from django.db import models
 
-
-class Theme(models.Model):
-    name = models.CharField(max_length=50,blank=True)
-
-class Step(models.Model):
-    number = models.IntegerField(default=0)
-    description = models.TextField(blank=True)
-
-
-class Material(models.Model):
-    name = models.CharField(max_length=128,blank=True)
-
-class Question(models.Model):
-    text = models.TextField(blank=True)
-
 class Challenge(models.Model):
-    name = models.CharField(max_length=128,blank=True)
-    description = models.TextField(blank=True)
-    about = models.TextField(blank=True)
-    theme = models.OneToOneField(Theme,related_name='challenge',null=True)
-    steps = models.ManyToManyField(Step, related_name="challenge", blank=True)
-    material = models.ManyToManyField(Material, related_name="challenge", blank=True)
-    questions = models.ManyToManyField(Question, related_name="challenge", blank=True)
-
+    name = models.TextField()
+    description = models.TextField()
+    how_to_make_it = models.TextField() # HTML
+    learn_more = models.TextField() # HTML
--- challenges/views.py
+++ challenges/views.py
@@ -1,35 +1,10 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Challenge
 
-def challenge_inspiration(request, challenge_id):
+def challenges(request):
+    challenges = Challenge.objects.all()
+    return render(request, 'challenges.html', {'challenges': challenges,})
+
+def challenge(request, challenge_id):
     challenge = get_object_or_404(Challenge, id=challenge_id)
-
-
-    template_values = {
-        'challenge':challenge,
-    }
-
-    return render(request, 'challenge_inspiration.html', template_values)
-
-
-def challenge_plan(request, challenge_id):
-    challenge = get_object_or_404(Challenge, id=challenge_id)
-
-
-    template_values = {
-        'challenge':challenge,
-    }
-
-    return render(request, 'challenge_plan.html', template_values)
-
-
-def challenge_build(request, challenge_id):
-    challenge = get_object_or_404(Challenge, id=challenge_id)
-
-
-    template_values = {
-        'challenge':challenge,
-    }
-
-    return render(request, 'challenge_build.html', template_values)
+    return render(request, 'challenge.html', {'challenge': challenge,})
