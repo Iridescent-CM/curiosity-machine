828a6b554f526be7b7044e80715b186488d14aff
Andrew Gorcester <andrew.gorcester@gmail.com>
2014-03-27 13:53:36 -0700

    add cmcomments commit and refactor challenges routes to reflect comment plans

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('challenges.views',
     url(r'^$', 'challenges', name='challenges'),
     url(r'^(?P<challenge_id>\d+)/$', 'challenge', name='challenge'),
+    url(r'^(?P<challenge_id>\d+)/(?P<username>[\w.@+-]+)/$', 'challenge_progress', name='challenge_progress'),
 )
--- challenges/views.py
+++ challenges/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from .models import Challenge, Progress
 
@@ -14,7 +15,14 @@ def challenge(request, challenge_id):
         Progress.objects.create(challenge=challenge, student=request.user)
         return HttpResponseRedirect('')
     try:
-        progress = Progress.objects.get(challenge=challenge, student=request.user)
-        return render(request, 'challenge_in_progress.html', {'challenge': challenge, 'progress': progress})
+        if Progress.objects.filter(challenge=challenge, student=request.user).exists():
+            return HttpResponseRedirect(reverse('challenges:challenge_progress', kwargs={'challenge_id': challenge.id, 'username': request.user.username,}))
     except Progress.DoesNotExist:
         return render(request, 'challenge.html', {'challenge': challenge,})
+
+# need security on this that only lets a student view his/her own progress
+def challenge_progress(request, challenge_id, username):
+    challenge = get_object_or_404(Challenge, id=challenge_id)
+    progress = get_object_or_404(Progress, challenge=challenge, student__username=username)
+
+    return render(request, 'challenge_in_progress.html', {'challenge': challenge, 'progress': progress,})
