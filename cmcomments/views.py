db7dd51ec54a3db1b2970e1948bcb8c3044e827f
karliswanson <karli@foundryinteractive.com>
2014-03-28 15:08:29 -0700

    Changing the flow of the comments view

--- cmcomments/views.py
+++ cmcomments/views.py
@@ -1,32 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.views.decorators.http import require_http_methods
 from challenges.models import Challenge, Progress
 from cmcomments.models import Comment
 from cmcomments.forms import CommentForm
 
 # refactor input into decorators
 # need security on this that only lets a student view his/her own progress
+@require_http_methods(["POST",])
 def comments(request, challenge_id, username):
     challenge = get_object_or_404(Challenge, id=challenge_id)
     progress = get_object_or_404(Progress, challenge=challenge, student__username=username)
 
-    if request.method == "POST":
-        form = CommentForm(data=request.POST) 
-        if form.is_valid():
-            text = form.cleaned_data['text']
-            comment = Comment()
-            comment.user = request.user
-            comment.text = text
-            comment.challenge_progress = progress
-            comment.save()
-        else:
-            template_values = {
-                'challenge': challenge, 
-                'progress': progress, 
-                'comment_form': form,
-            }
-            return render(request, 'challenge_in_progress.html', template_values)
+    form = CommentForm(data=request.POST) 
+    if form.is_valid():
+        text = form.cleaned_data['text']
+        comment = Comment()
+        comment.user = request.user
+        comment.text = text
+        comment.challenge_progress = progress
+        comment.save()
 
     return HttpResponseRedirect(reverse('challenges:challenge_progress', kwargs={'challenge_id': challenge.id, 'username': username,}))
     
