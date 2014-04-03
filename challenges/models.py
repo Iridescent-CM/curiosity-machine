e268a6ed00d3870c3c25706309c270095ce74682
karliswanson <karli@foundryinteractive.com>
2014-04-03 14:23:35 -0700

    Only allowing students for student fields and mentors for mentor fields

--- challenges/models.py
+++ challenges/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 
 class Theme(models.Model):
     name = models.TextField()
@@ -15,10 +16,24 @@ class Challenge(models.Model):
     mentor = models.ForeignKey(User, related_name='mentored_challenges', null=True)
     theme = models.ForeignKey(Theme, null=True)
 
+    def save(self, *args, **kwargs):
+        if not self.mentor.profile.is_mentor:
+            raise ValidationError("The mentor of a challenge can not be a student")
+        else:
+            super(Challenge, self).save(*args, **kwargs)
+
 class Progress(models.Model):
     challenge = models.ForeignKey(Challenge)
     student = models.ForeignKey(User)
     started = models.DateTimeField(default=now)
+
+    def save(self, *args, **kwargs):
+        if Progress.objects.filter(challenge=self.challenge, student=self.student).exists():
+            raise ValidationError("There is already progress by this student on this challenge")
+        elif self.student.profile.is_mentor:
+            raise ValidationError("Mentors can not start a challenge")
+        else:
+            super(Progress, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('challenges:challenge_progress', kwargs={'challenge_id': self.challenge_id, 'username': self.student.username,})
