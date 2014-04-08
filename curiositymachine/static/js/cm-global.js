6e9cf7a2a844c82e4570b1a3408b97ca6a785738
Jim <jim@foundryinteractive.com>
2014-04-07 19:25:04 -0700

    added carousels for displaying challenges and themes. switched to responsive rather than breakpoint content container

--- curiositymachine/static/js/cm-global.js
+++ curiositymachine/static/js/cm-global.js
@@ -12,12 +12,47 @@ $(document).ready(function() {
     }
   });
 
+
+//=======================
+//bootstrapy stuff
+//====================
   //this is so we can reload content into the same modal.
   //re-initilaizes the modal when hidden
   $('body').on('hidden.bs.modal', '.modal', function () {
     $(this).removeData('bs.modal');
   });
 
+  //actiavate tabs
+  $('body').on('click', '.nav-tabs a', function(e) {
+      e.preventDefault();
+      $(this).tab('show');
+  });
+//======= end bootstrappy stuff
+
+
+  $(".panel-carousel").owlCarousel({
+    items : 4,
+    itemsCustom : false,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,2],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false
+  });
+
+  $('.theme-carousel').owlCarousel({
+    items : 6,
+    itemsCustom : false,
+    itemsDesktop : [1199,6],
+    itemsDesktopSmall : [980,4],
+    itemsTablet: [768,3],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false
+  });
 
 });
 
