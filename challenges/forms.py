7d96a4734230ec5c1bff796b3dc41f1e8e7dff9a
karliswanson <karli@foundryinteractive.com>
2014-04-17 11:27:11 -0700

    Adding videos for describing a challenge

---
+++ challenges/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from cmcomments.forms import FilePickerURLField
+
+
+class ChallengeVideoForm(forms.Form):
+    video_filepicker_url = FilePickerURLField(mimetypes="video/*", openTo='VIDEO')
--- challenges/views.py
+++ challenges/views.py
@@ -3,10 +3,15 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.exceptions import ValidationError
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+import django_rq
 
 from .models import Challenge, Progress, Theme
 from cmcomments.forms import CommentForm
 from curiositymachine.decorators import mentor_or_current_student
+from videos.models import Video
+from videos.views import upload_filepicker_video
+from .forms import ChallengeVideoForm
 
 def challenges(request):
     challenges = Challenge.objects.all()
@@ -29,7 +34,7 @@ def challenge(request, challenge_id):
     if request.user.is_authenticated() and Progress.objects.filter(challenge=challenge, student=request.user).exists():
         return HttpResponseRedirect(reverse('challenges:challenge_progress', kwargs={'challenge_id': challenge.id, 'username': request.user.username,}))
     else:
-        return render(request, 'challenge.html', {'challenge': challenge,})
+        return render(request, 'challenge.html', {'challenge': challenge, 'video_form': ChallengeVideoForm()})
 
 # TODO: refactor input into decorators
 @login_required
@@ -39,3 +44,16 @@ def challenge_progress(request, challenge_id, username):
     progress = get_object_or_404(Progress, challenge=challenge, student__username=username)
 
     return render(request, 'challenge_in_progress.html', {'challenge': challenge, 'progress': progress, 'comment_form': CommentForm()})
+
+@login_required
+@require_http_methods(["POST",])
+def upload_video(request, challenge_id):
+    challenge = get_object_or_404(Challenge, id=challenge_id)
+    form = ChallengeVideoForm(data=request.POST)
+    if form.is_valid():
+        video = Video.objects.create(video=form.cleaned_data['video_filepicker_url'])
+        challenge.video = video
+        challenge.save()
+        django_rq.enqueue(upload_filepicker_video, challenge)
+    return HttpResponseRedirect(reverse('challenges:challenge', kwargs={'challenge_id': challenge.id,}))
+
