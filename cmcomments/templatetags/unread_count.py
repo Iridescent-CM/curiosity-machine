546679432f56a947ce6dfec8ab9ad27b089fa389
karliswanson <karli@foundryinteractive.com>
2014-04-23 15:08:57 -0700

    Switching the mentor field to the progress

--- cmcomments/templatetags/unread_count.py
+++ cmcomments/templatetags/unread_count.py
@@ -8,5 +8,5 @@ from challenges.models import Progress
 def unread_count(progress, user):
     if progress is None or user is None:
         return None
-    return len(progress.get_unread_comments(user))
+    return len(progress.get_unread_comments_for_user(user))
 
