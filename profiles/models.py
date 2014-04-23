84e4dac692853e9b17b25db6aad02cc5692e98e8
karliswanson <karli@foundryinteractive.com>
2014-04-23 12:53:57 -0700

    Making django admin more readable

--- profiles/models.py
+++ profiles/models.py
@@ -25,6 +25,9 @@ class Profile(models.Model):
     def is_student(self):
         return not self.is_mentor
 
+    def __str__(self):
+        return "Profile: id={}, user={}".format(self.id, self.user.username)
+
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
