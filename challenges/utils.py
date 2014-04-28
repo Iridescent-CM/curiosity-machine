34276546647e675a2619488a203fbb073e2b2a36
Andrew Gorcester <andrew.gorcester@gmail.com>
2014-04-28 14:46:05 -0700

    fix bug in get_stage_for_progress

--- challenges/utils.py
+++ challenges/utils.py
@@ -3,7 +3,7 @@ from .models import Stage
 def get_stage_for_progress(progress, default=Stage.plan):
     try:
         # find most recent activity and use its stage
-        stage = progress.comments.order_by("-created")[0]
+        return progress.comments.order_by("-created")[0].stage
     except IndexError:
         # if no activity, use default
         return default
