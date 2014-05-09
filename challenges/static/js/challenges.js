83cd4c68a53d96ea3d5c86317ca31b1d359ac5d7
karliswanson <karli@foundryinteractive.com>
2014-05-09 14:09:13 -0700

    Only allowing comment submit once the form is full

--- challenges/static/js/challenges.js
+++ challenges/static/js/challenges.js
@@ -24,4 +24,27 @@ $(document).ready(function() {
 
   });
 
+  $('.comment-form').find('input[type=filepicker-dragdrop]').on('change', function(e) {
+    disable_submit_until_filled(this);
+  });
+
+  $('.comment-form textarea').on('keyup', function(e) {
+    disable_submit_until_filled(this);
+  });
+
 });
+
+function disable_submit_until_filled(element) {
+  var empty = false;
+  var form = element.form;
+  $(form).find(':input').each(function() {
+      if ((this.type == 'text'|| this.type == 'textarea') && $(this).val() == '') {
+          empty = true;
+      }
+  });
+  if (empty) {
+      $(form).find('input[type=submit]').attr('disabled', 'disabled');
+  } else {
+      $(form).find('input[type=submit]').removeAttr('disabled');
+  }
+}
