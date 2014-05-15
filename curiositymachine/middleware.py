e64ec5cd69da988ec92cba1af0b5f4cf8461be87
karliswanson <karli@foundryinteractive.com>
2014-05-15 10:09:16 -0700

    Removing age requirement for sandboxing

--- curiositymachine/middleware.py
+++ curiositymachine/middleware.py
@@ -6,7 +6,7 @@ class UnderageStudentSandboxMiddleware:
     Middleware that sandboxes students under 13 away from the rest of the site.
     """
     def process_request(self, request):
-        if request.user.is_authenticated() and request.user.profile.age < 13 and not request.user.profile.approved:
-            # if under 13 and not yet approved
+        if request.user.is_authenticated() and not request.user.profile.approved:
+            # if not yet approved
             if request.path_info not in ['/logout', '/logout/', reverse('profiles:underage_student')]:
                 return HttpResponseRedirect(reverse('profiles:underage_student'))
