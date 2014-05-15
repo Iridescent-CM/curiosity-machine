c458e4ac927dc9c4743a5c90de75da46d5b6683c
karliswanson <karli@foundryinteractive.com>
2014-05-15 15:43:04 -0700

    Not sandboxing if user is staff

--- curiositymachine/middleware.py
+++ curiositymachine/middleware.py
@@ -6,7 +6,7 @@ class UnderageStudentSandboxMiddleware:
     Middleware that sandboxes students under 13 away from the rest of the site.
     """
     def process_request(self, request):
-        if request.user.is_authenticated() and not request.user.profile.approved:
+        if request.user.is_authenticated() and not request.user.is_staff and not request.user.profile.approved:
             # if not yet approved
             if request.path_info not in ['/logout', '/logout/', reverse('profiles:underage_student')]:
                 return HttpResponseRedirect(reverse('profiles:underage_student'))
