14d6111b83227a8914bdff943f10f644437c7e3e
Andrew Gorcester <andrew.gorcester@gmail.com>
2014-05-16 15:59:22 -0700

    training comment support with tests

--- training/tests.py
+++ training/tests.py
@@ -1,14 +1,19 @@
 import pytest
-from .models import Module
+from .models import Module, Comment
 from .views import module as module_view
+from .views import comments
 from profiles.tests import student, mentor
 
 @pytest.fixture
 def module():
     return Module.objects.create(id=1)
 
+@pytest.fixture
+def training_comment(module, mentor):
+    return module.comments.create(user=mentor)
+
 @pytest.mark.django_db
-def test_training_response_code(rf, student, mentor, module):
+def test_module_response_code(rf, student, mentor, module):
     request = rf.get('/training/1/')
     request.user = student
     response = module_view(request, module.id)
@@ -24,3 +29,34 @@ def test_training_response_code(rf, student, mentor, module):
     request.user = mentor
     response = module_view(request, module.id)
     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_comments_blocks_student(rf, student, mentor, module):
+    request = rf.post('/training/1/1/', {'text': 'test_text'})
+    request.user = student
+    response = comments(request, module.id)
+    assert response.status_code == 403
+
+@pytest.mark.django_db
+def test_comments_top_level(rf, student, mentor, module):
+    assert Comment.objects.count() == 0
+    request = rf.post('/training/1/comments/', {'text': 'test_text'})
+    request.user = mentor
+    response = comments(request, module.id)
+    assert response.status_code == 204
+    assert Comment.objects.count() == 1
+    comment = Comment.objects.first()
+    assert comment.text == 'test_text'
+    assert not comment.thread_id
+
+@pytest.mark.django_db
+def test_comments_with_thread_id(rf, student, mentor, module, training_comment):
+    assert Comment.objects.count() == 1
+    request = rf.post('/training/1/comments/1/', {'text': 'test_text'})
+    request.user = mentor
+    response = comments(request, module.id, training_comment.id)
+    assert response.status_code == 204
+    assert Comment.objects.count() == 2
+    comment = Comment.objects.first()
+    assert comment.text == 'test_text'
+    assert comment.thread_id == training_comment.id
--- training/views.py
+++ training/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST, HttpResponse
 from curiositymachine.decorators import mentor_only
-from .models import Module
+from .models import Module, Comment
+from .forms import CommentForm
 
+@login_required
 @mentor_only
 def module(request, module_id):
     module = get_object_or_404(Module, id=module_id)
@@ -12,3 +16,17 @@ def module(request, module_id):
     else:
         threads = module.comments.filter(user__profile__approved=False, thread__isnull=True)
     return render(request, "training.html", {"module": module, "threads": threads})
+
+@require_POST
+@login_required
+@mentor_only
+def comments(request, module_id, thread_id=None):
+    module = get_object_or_404(Module, id=module_id)
+    thread = get_object_or_404(module.comments, id=thread_id, thread__isnull=True) if thread_id else None # if thread_id is specified, an invalid thread_id (top-level-comment id) is a 404, but passing in no thread_id at all is legal
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = Comment(user=request.user, text=form.cleaned_data['text'], module=module, thread=thread)
+        comment.save()
+
+    return HttpResponse(status=204)
