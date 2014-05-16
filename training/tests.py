c53656363431a5191adeb8b6b62eec2b176cc79b
Andrew Gorcester <andrew.gorcester@gmail.com>
2014-05-16 14:36:11 -0700

    tests for basic training view

--- training/tests.py
+++ training/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from .models import Module
+from .views import module
+from profile.tests import student, mentor
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_training_response_code(student, mentor, module):
+    request = rf.get('/training/1/')
+    request.user = student
+    response = module(request, module.id)
+    assert response.status_code == 403
+
+    request = rf.get('/training/1/')
+    request.user = mentor
+    response = module(request, module.id)
+    assert response.status_code == 200
+
+    mentor.profile.approved = False
+    request = rf.get('/training/1/')
+    request.user = mentor
+    response = module(request, module.id)
+    assert response.status_code == 200
