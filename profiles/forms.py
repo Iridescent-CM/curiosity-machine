911f419896913c27ffe8502e490938b7c149377a
karliswanson <karli@foundryinteractive.com>
2014-05-22 14:36:42 -0700

    Styling join page when there are errors
    Adding another template for a non modal page to show when there are
    form validation errors

--- profiles/forms.py
+++ profiles/forms.py
@@ -28,7 +28,7 @@ class ProfileFormBase(forms.Form):
         password = cleaned_data.get("password")
         confirm_password = cleaned_data.get("confirm_password")
         if password != confirm_password:
-            raise forms.ValidationError("Passwords do not match.")
+            self.add_error('password', "Passwords do not match.")
         return cleaned_data
 
     def clean_password(self):
