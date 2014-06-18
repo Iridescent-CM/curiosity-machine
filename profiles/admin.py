6a5d89e76678bf2791adec54828cfde7e04f9388
Andrew Gorcester <andrew.gorcester@gmail.com>
2014-06-18 14:52:17 -0700

    profile shows restricted image field like all other image admins (to avoid slow or timeout page loads when there are many images registered)

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from images.models import Image
+
 from .models import Profile
 
 admin.site.unregister(User)
@@ -8,8 +10,21 @@ admin.site.unregister(User)
 class ProfileInline(admin.StackedInline):
     model = Profile
 
-class ProfileAdmin(UserAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if request.method == 'GET':
+            if db_field.name == 'image':
+                if request._obj_ is not None and request._obj_.profile.image is not None:
+                    kwargs["queryset"] = Image.objects.filter(source_url = request._obj_.profile.image.source_url)
+                else:
+                    kwargs["queryset"] = Image.objects.none()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+class UserAdminWithProfile(UserAdmin):
     inlines = [ ProfileInline, ]
+
+    def get_form(self, request, obj=None, **kwargs):
+        request._obj_ = obj
+        return super().get_form(request, obj, **kwargs)
 
     def get_formsets(self, request, obj=None):
         for inline in self.get_inline_instances(request, obj):
@@ -18,4 +33,4 @@ class ProfileAdmin(UserAdmin):
                 continue
             yield inline.get_formset(request, obj)
 
-admin.site.register(User, ProfileAdmin)
+admin.site.register(User, UserAdminWithProfile)
