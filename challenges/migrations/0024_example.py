95c9b365d3fdabff3387ff57f6414bcabc98707e
Andrew Gorcester <andrew.gorcester@gmail.com>
2014-06-23 14:47:25 -0700

    amended migration

--- challenges/migrations/0024_example.py
+++ challenges/migrations/0024_example.py
@@ -6,8 +6,8 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('images', '__first__'),
         ('videos', '__first__'),
+        ('images', '__first__'),
         ('challenges', '0023_auto_20140528_1434'),
     ]
 
@@ -15,12 +15,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Example',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
                 ('challenge', models.ForeignKey(to_field='id', to='challenges.Challenge')),
-                ('progress', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='challenges.Progress', null=True, to_field='id', blank=True, help_text="An optional association with a specific student's progress on a challenge.")),
-                ('name', models.TextField(blank=True, help_text="The student's username in plain text. This can be left blank if a progress is set, in which case the progress's student username will be automatically used instead.")),
-                ('image', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='images.Image', null=True, to_field='id', blank=True, help_text='An image to display in the gallery. If a video is also set, this will be the thumbnail. Each example must have an image or a video, or both, to be displayed correctly.')),
-                ('video', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='videos.Video', null=True, to_field='id', blank=True, help_text='Each example must have an image or a video, or both, to be displayed correctly.')),
+                ('progress', models.ForeignKey(to='challenges.Progress', to_field='id', on_delete=django.db.models.deletion.SET_NULL, null=True, blank=True, help_text="An optional association with a specific student's progress on a challenge.")),
+                ('_name', models.TextField(db_column='name', verbose_name='name', help_text="The student's username in plain text. This can be left blank if a progress is set, in which case the progress's student username will be automatically used instead.", blank=True)),
+                ('image', models.ForeignKey(to='images.Image', to_field='id', on_delete=django.db.models.deletion.SET_NULL, null=True, blank=True, help_text='An image to display in the gallery. If a video is also set, this will be the thumbnail. Each example must have an image or a video, or both, to be displayed correctly.')),
+                ('video', models.ForeignKey(to='videos.Video', to_field='id', on_delete=django.db.models.deletion.SET_NULL, null=True, blank=True, help_text='Each example must have an image or a video, or both, to be displayed correctly.')),
             ],
             options={
             },
