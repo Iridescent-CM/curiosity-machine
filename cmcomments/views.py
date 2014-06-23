756844c3a053e41d55084f9359b4f378d8c2a364
Andrew Gorcester <andrew.gorcester@gmail.com>
2014-06-23 15:52:45 -0700

    mentors can feature examples

--- cmcomments/views.py
+++ cmcomments/views.py
@@ -4,10 +4,11 @@ from django.http import Http404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.conf import settings
-from challenges.models import Challenge, Progress, Stage
+from challenges.models import Challenge, Progress, Stage, Example
+from django.contrib import messages
 from cmcomments.models import Comment
 from cmcomments.forms import CommentForm
-from curiositymachine.decorators import mentor_or_current_student
+from curiositymachine.decorators import mentor_or_current_student, mentor_only
 from videos.models import Video
 from images.models import Image
 import django_rq
@@ -31,4 +32,24 @@ def comments(request, challenge_id, username, stage):
         comment.save()
     #TODO: add some way to handle form.errors, for instance converting it into a JSON API
 
-    return HttpResponseRedirect(reverse('challenges:challenge_progress', kwargs={'challenge_id': challenge.id, 'username': username}))
+    return HttpResponseRedirect(reverse('challenges:challenge_progress', kwargs={'challenge_id': challenge.id, 'username': username, 'stage': stage}))
+
+# Any POST to this features a student's progress in the "see what other kids built" section on the inspiration page.
+# Only one comment from a student for a specific challenge can be featured at a time; multiple POSTs will override previous selections.
+# This only really makes sense if the comment has either an image or a video, but there's nothing stopping a forged POST request for a comment without either of those.
+@require_POST
+@mentor_only
+def feature_as_example(request, challenge_id, username, stage, comment_id): # "stage" is only used for the redirect
+    progress = get_object_or_404(Progress, challenge_id=challenge_id, student__username=username)
+    comment = get_object_or_404(progress.comments, id=comment_id)
+
+    if Example.objects.filter(progress=progress, image=comment.image, video=comment.video):
+        messages.error(request, "This media was already featured, so nothing was changed.")
+    elif Example.objects.filter(progress=progress).exists():
+        Example.objects.filter(progress=progress).delete()
+        messages.success(request, "{}'s previously featured media was replaced.".format(progress.student))
+    else:
+        messages.success(request, "{}'s uploaded media was featured on the inspiration page.".format(progress.student))
+    Example.objects.create(challenge=progress.challenge, progress=progress, image=comment.image, video=comment.video)
+
+    return HttpResponseRedirect(reverse('challenges:challenge_progress', kwargs={'challenge_id': challenge_id, 'username': username, 'stage': stage}))
