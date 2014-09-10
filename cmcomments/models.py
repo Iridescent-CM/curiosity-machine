80f037673aabfff781304e56d1851979575c3925
Stephen Paul Suarez <devpopol@gmail.com>
2014-09-11 06:47:41 +0800

    https://trello.com/c/MOhiIfFu/86-email-notification-for-mentors-change-when-we-apply-the-direct-account-creation
    email fixes has been applied

--- cmcomments/models.py
+++ cmcomments/models.py
@@ -6,6 +6,9 @@ from videos.models import Video
 from images.models import Image
 from django.db.models.signals import post_save
 from cmemails import deliver_email
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Comment(models.Model):
     challenge_progress = models.ForeignKey(Progress, related_name='comments')
@@ -29,11 +32,12 @@ class Comment(models.Model):
 
     def email_student_completed(self):
         if self.challenge_progress.mentor:
-            return deliver_email('student_completed', self.challenge_progress.mentor.profile, student=self.challenge_progress.student.profile)
+            return deliver_email('student_completed', self.challenge_progress.mentor.profile, student=self.challenge_progress.student.profile, progress=self.challenge_progress)
 
 def create_comment(sender, instance, created, **kwargs):
     if created:
-        if instance.stage == Stage.reflect.value:
+        print(instance.image)
+        if instance.stage == Stage.reflect.value and instance.image:
             instance.email_student_completed()
 
 post_save.connect(create_comment, sender=Comment)
