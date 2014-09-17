b7257b6275ca4e70372ba0f3dda2c66103a47002
Camila Achutti <camila.achutti@gmail.com>
2014-09-17 09:35:31 -0300

    btd

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('challenges.views',
     url(r'^$', 'challenges', name='challenges'),
-    url(r'^(?P<challenge_id>\d+)/', 'challenge', name='challenge'),
+    url(r'^(?P<challenge_id>\d+)/$', 'challenge', name='challenge'),
     url(r'^(?P<challenge_id>\d+)/plan/$', 'plan_guest', name='plan_guest'),
      url(r'^(?P<challenge_id>\d+)/build/$', 'build_guest', name='build_guest'),
     url(r'^(?P<challenge_id>\d+)/(?P<username>[\w.@+-]+)/$', 'challenge_progress', name='challenge_progress'),
