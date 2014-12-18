5f556148f8ebd1013c5e3a8e96a029b49ff88e73
Mike Larsson <mikelarssonftw@gmail.com>
2014-12-18 14:57:11 -0500

    Update root_redirect to not assume non-mentor is student, with tests

--- curiositymachine/views.py
+++ curiositymachine/views.py
@@ -3,7 +3,8 @@ from django.core.urlresolvers import reverse
 
 def root_redirect(request):
     # redirect to home if logged in unless you are a student with no challenges
-    if request.user.is_authenticated() and (request.user.profile.is_mentor or request.user.progresses.exists()):
+    if (request.user.is_authenticated()
+            and (not request.user.profile.is_student or request.user.progresses.exists())):
         return HttpResponseRedirect(reverse('profiles:home'))
     else:
         return HttpResponseRedirect(reverse('challenges:challenges'))
