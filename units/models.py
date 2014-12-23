357e56e10211e138106e17fb86bf18f672a1586b
Stephen Paul Suarez <devpopol@gmail.com>
2014-12-23 11:38:38 +0800

    str and repr

--- units/models.py
+++ units/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
+
 class Unit(models.Model):
-	name = models.TextField()
+    name = models.TextField()
+
+    def __str__(self):
+        return "Unit: id={}, name={}".format(self.id, self.name)
+
+    def __repr__(self):
+        return "Unit: id={}, name={}".format(self.id, self.name)
