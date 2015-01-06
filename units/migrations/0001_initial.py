6a4182a3f78ae8956a4d58b8c4098a2c43bb5bae
Mike Larsson <mikelarssonftw@gmail.com>
2015-01-06 17:35:30 -0500

    Squashing units migrations so far into a single one

--- units/migrations/0001_initial.py
+++ units/migrations/0001_initial.py
@@ -1,10 +1,12 @@
 # encoding: utf8
 from django.db import models, migrations
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('images', '__first__'),
         ('challenges', '__first__'),
     ]
 
@@ -12,10 +14,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Unit',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.TextField(help_text='name of the unit')),
+                ('description', models.TextField(null=True, blank=True, help_text='blurb for the unit')),
+                ('overview', models.TextField(null=True, blank=True, help_text='overview for the unit')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
+                ('standards_alignment_image', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='images.Image', null=True, blank=True, to_field='id')),
                 ('challenges', models.ManyToManyField(to='challenges.Challenge')),
             ],
             options={
