b438b41615b96c340daa2fc949cba2cefe5dfb94
Stephen Paul Suarez <devpopol@gmail.com>
2015-01-13 04:00:45 +0800

    add migration

--- units/migrations/0001_initial.py
+++ units/migrations/0001_initial.py
@@ -28,16 +28,5 @@ class Migration(migrations.Migration):
             },
             bases=(models.Model,),
         ),
-        migrations.CreateModel(
-            name='Resource',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('name', models.TextField(help_text='name of the unit')),
-                ('file', models.ForeignKey(blank=True, null=True, to_field='id', to='files.File', on_delete=django.db.models.deletion.SET_NULL)),
-                ('unit', models.ForeignKey(to='units.Unit', to_field='id')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
+        
     ]
