566ac6254fbaf25087cfdec571dd7f4598aa910b
Mike Larsson <mikelarssonftw@gmail.com>
2015-01-20 17:35:46 -0500

    List all images in units dropdowns

--- units/admin.py
+++ units/admin.py
@@ -18,19 +18,4 @@ class UnitAdmin(admin.ModelAdmin):
         request._obj_ = obj
         return super(UnitAdmin, self).get_form(request, obj, **kwargs)
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if request.method == 'GET':
-            if db_field.name == 'standards_alignment_image':
-                if request._obj_ is not None and request._obj_.standards_alignment_image is not None:
-                    kwargs["queryset"] = Image.objects.filter(source_url = request._obj_.standards_alignment_image.source_url)
-                else:
-                    kwargs["queryset"] = Image.objects.none()
-            if db_field.name == 'image':
-                if request._obj_ is not None and request._obj_.image is not None:
-                    kwargs["queryset"] = Image.objects.filter(source_url = request._obj_.image.source_url)
-                else:
-                    kwargs["queryset"] = Image.objects.none()
-        return super(UnitAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
-
-
 admin.site.register(Unit, UnitAdmin)
