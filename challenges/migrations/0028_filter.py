fee5d230f9fa52d88b772fdac69e2afc5e69c20b
Mike Larsson <mikelarssonftw@gmail.com>
2015-02-02 13:21:05 -0500

    Consolidate migrations and remove conflict

--- challenges/migrations/0028_filter.py
+++ challenges/migrations/0028_filter.py
@@ -5,17 +5,19 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('challenges', '0026_auto_20140630_0708'),
+        ('challenges', '0027_challenge_mentor_guide'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Filter',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('name', models.TextField(help_text='name of the filter')),
+                ('color', models.TextField(help_text='a hex color like 44b1cc', null=True, blank=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
+                ('visible', models.BooleanField(db_index=True, default=False)),
                 ('challenges', models.ManyToManyField(to='challenges.Challenge')),
             ],
             options={
