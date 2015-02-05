828d2377801577b86fd7acb0a9c999357e512a89
System Administrator <camila@iridescentlearning.org>
2015-02-05 20:00:35 -0200

    cleam migration

--- profiles/migrations/0017_auto_20150205_1316.py
+++ profiles/migrations/0017_auto_20150205_1316.py
@@ -19,34 +19,4 @@ class Migration(migrations.Migration):
             field=models.BooleanField(default=False),
             preserve_default=True,
         ),
-        migrations.AlterField(
-            model_name='profile',
-            name='about_me_image',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='about_me_image', to='images.Image'),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='profile',
-            name='about_me_video',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='about_me_video', to='videos.Video'),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='profile',
-            name='about_research_image',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='about_research_image', to='images.Image'),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='profile',
-            name='about_research_video',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='about_research_video', to='videos.Video'),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='profile',
-            name='user',
-            field=models.OneToOneField(to=settings.AUTH_USER_MODEL, related_name='profile'),
-            preserve_default=True,
-        ),
     ]
