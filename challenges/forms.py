d154a28829239759c2158d9e0f6294087db36d05
Mike Larsson <mikelarssonftw@gmail.com>
2015-02-10 15:10:33 -0500

    Validate hex color length and correct missing #

--- challenges/forms.py
+++ challenges/forms.py
@@ -25,3 +25,9 @@ class FilterForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput, required=True, help_text="Name")
     color = forms.CharField(widget=forms.TextInput, required=True, validators=[validate_color], help_text=mark_safe("Enter the background color in hex format. for example: #ffffff<br><br>Here are the brand colors for reference:<br> Blue: <strong>#44b1f5</strong> Green: <strong>#84af49</strong> Orange: <strong>#f16243</strong> Teal: <strong>#1bb2c4</strong> Yellow: <strong>#f1ac43</strong><br>gray-darker: <strong>#222222</strong> gray-dark: <Strong>#333333</strong> gray: <strong>#555555</strong> gray-light: <strong>#999999</strong> gray-lighter: <strong>#eee</strong>"))
     visible = forms.BooleanField()
+
+    def clean_color(self):
+        color = self.cleaned_data.get('color')
+        if color[0] != '#':
+            color = '#' + color
+        return color
--- challenges/validators.py
+++ challenges/validators.py
@@ -3,6 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 import re
 
 def validate_color(value):
-    regex = r"^#?[0-9a-fA-F]+$"
+    regex = r"^#?[0-9a-fA-F]{6}$"
     if not re.match(regex, value):
     	raise ValidationError(_('Invalid Color. Use a hex color like #44b1cc.'), code='invalid')
