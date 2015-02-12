f65db75350e6eea845ab65e04877217dafc0601c
System Administrator <camila@iridescentlearning.org>
2015-02-12 17:05:23 -0200

    fix tests

--- challenges/tests.py
+++ challenges/tests.py
@@ -59,7 +59,7 @@ def test_challenges_render_challenges(client, challenge, student):
 
 @pytest.mark.django_db
 def test_challenges_filters_by_name(client, challenge, challenge2, theme, student):
-    challenge.theme = theme
+    challenge.themes.add(theme)
     challenge.save()
 
     response = client.get('/challenges/')
@@ -78,7 +78,7 @@ def test_ajax_challenges(client, loggedInStudent, challenge):
 
 @pytest.mark.django_db
 def test_ajax_challenges_filters_by_name(client, loggedInStudent, challenge, challenge2, theme):
-    challenge.theme = theme
+    challenge.themes.add(theme)
     challenge.save()
 
     response = client.get('/challenges/ajax_challenges')
