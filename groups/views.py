c4625b7f9565b263ab2f2657a80c1ffe3e309178
Stephen Paul Suarez <devpopol@gmail.com>
2015-02-24 02:15:44 +0800

    use feature flag

--- groups/views.py
+++ groups/views.py
@@ -2,11 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Group, Role
+from curiositymachine.decorators import feature_flag
 
+@feature_flag('enable_groups')
 def groups(request):
 	groups = Group.objects.all()
 	return render(request, 'groups.html', {'groups': groups})
 
+@feature_flag('enable_groups')
 def group(request, group_id): 
 	group = get_object_or_404(Group, id=group_id)
 	return render(request, 'group.html', {'group': group})
