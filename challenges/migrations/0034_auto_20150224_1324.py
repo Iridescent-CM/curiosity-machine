b772d009ee95ba6448358ab425dbaeb462bcddab
Mike Larsson <mikelarssonftw@gmail.com>
2015-02-24 20:07:15 -0500

    Migrate theme to themes with raw sql

--- challenges/migrations/0034_auto_20150224_1324.py
+++ challenges/migrations/0034_auto_20150224_1324.py
@@ -1,11 +1,15 @@
 # encoding: utf8
-from django.db import models, migrations
+from django.db import connection, migrations
 
 def copy_themes(apps, schema_editor):
-    Challenge = apps.get_model("challenges", "Challenge")
-    for challenge in Challenge.objects.filter(theme__isnull=False).select_related('theme'):
-        challenge.themes.add(challenge.theme)
-        challenge.save()
+    cursor = connection.cursor()
+    cursor.execute("""
+        INSERT INTO challenges_challenge_themes (challenge_id, theme_id)
+        SELECT challenges_challenge.id, challenges_theme.id
+        FROM challenges_challenge 
+            JOIN challenges_theme
+            ON challenges_challenge.theme_id = challenges_theme.id
+    """)
 
 def noop(apps, schema_editor):
     pass
