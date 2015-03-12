904d05096ca6f93ee93fc65ff73fe58ff7294fa7
Mike Larsson <mikelarssonftw@gmail.com>
2015-03-12 11:01:08 -0400

    Forgot X

--- curiositymachine/helpers.py
+++ curiositymachine/helpers.py
@@ -1,7 +1,7 @@
 import string
 import random
 
-CHARS = 'bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWZ0123456789'
+CHARS = 'bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ0123456789'
 
 def random_string(length=5):
     return "".join(random.choice(list(CHARS)) for _ in range(length))
