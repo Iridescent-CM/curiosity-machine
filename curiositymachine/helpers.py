cb56a034de950b13500dc34dafbd03b40294a008
Mike Larsson <mikelarssonftw@gmail.com>
2015-03-12 10:59:14 -0400

    Create codes from consonants and digits to avoid accidental word creation

--- curiositymachine/helpers.py
+++ curiositymachine/helpers.py
@@ -1,6 +1,7 @@
 import string
 import random
 
+CHARS = 'bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWZ0123456789'
+
 def random_string(length=5):
-    char_list = list(map(lambda x: str(x).upper(), list(string.ascii_lowercase) + list(range(0,10))))
-    return "".join(random.choice(char_list) for _ in range(length))
+    return "".join(random.choice(list(CHARS)) for _ in range(length))
