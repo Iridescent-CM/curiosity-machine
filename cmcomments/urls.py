b6ba039e9030565d1369f21815dd11e62466b79a
Stephen Paul Suarez <devpopol@gmail.com>
2015-04-23 04:09:11 +0800

    routes preparation for html/json support

--- cmcomments/urls.py
+++ cmcomments/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('cmcomments.views',
-    url(r'^\\.(?P<format>html|json)$', 'comments', name='comments'),
+    url(r'^(?P<format>html|json)?$', 'comments', name='comments'),
     url(r'^(?P<comment_id>\d+)/$', 'feature_as_example', name='feature_as_example'),
 )
