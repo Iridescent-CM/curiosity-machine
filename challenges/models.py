0d24fa8d928d500e6abda793d6b22a420c3029b0
Mike Larsson <mikelarssonftw@gmail.com>
2015-04-28 12:36:46 -0400

    remove extraneous import

--- challenges/models.py
+++ challenges/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import Q
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 from django.core.urlresolvers import reverse
