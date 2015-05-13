13136489ab10478e7c4b0a1610f1580036d46036
Mike Larsson <mikelarssonftw@gmail.com>
2015-05-13 15:33:26 -0400

    Adding more tests

--- profiles/tests/test_parent.py
+++ profiles/tests/test_parent.py
@@ -44,6 +44,32 @@ def test_creates_user_with_profile():
     assert f.is_valid()
     user = f.save()
     assert hasattr(user, "profile")
+    assert User.objects.all().count() == 1
+    assert models.Profile.objects.all().count() == 1
+
+@pytest.mark.django_db
+def test_modifies_exisiting_user_and_profile():
+    f = forms.parent.ParentUserAndProfileForm(data={
+        'password': '123123',
+        'confirm_password': '123123',
+        'username': 'example',
+        'email': 'email@example.com',
+        'city': 'mycity',
+        'is_student': True
+    })
+    user = f.save()
+    f = forms.parent.ParentUserAndProfileForm(
+        instance=user,
+        data={
+            'email': 'new@example.com',
+            'city': 'newcity'
+        }
+    )
+    user = f.save()
+    assert user.email == 'new@example.com'
+    assert user.profile.city == 'newcity'
+    assert User.objects.all().count() == 1
+    assert models.Profile.objects.all().count() == 1
 
 @pytest.mark.django_db
 def test_sets_is_parent_flag():
@@ -56,3 +82,19 @@ def test_sets_is_parent_flag():
     })
     user = f.save()
     assert user.profile.is_parent
+
+@pytest.mark.django_db
+def test_form_ignores_unspecified_profile_fields():
+    f = forms.parent.ParentUserAndProfileForm(data={
+        'password': '123123',
+        'confirm_password': '123123',
+        'username': 'example',
+        'email': 'email@example.com',
+        'city': 'mycity',
+        'is_student': True,
+        'title': 'title'
+    })
+    user = f.save()
+    assert not user.profile.is_student
+    assert user.profile.title == ''
+
