c1e7e16ed59dead4dfaad3e3b854fdc2e74c3526
Mike Larsson <mikelarssonftw@gmail.com>
2015-05-19 17:15:02 -0400

    Add ParentConnection creation form, with username field refactor

--- groups/forms.py
+++ groups/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django import forms
+from curiositymachine.forms import StudentUsernamesField
 from . import models
-import re
 
 class GroupForm(forms.ModelForm):
     class Meta:
@@ -68,32 +68,9 @@ class ManageInvitationsForm(forms.ModelForm):
             invites.delete()
         return self.instance
 
-class ListField(forms.Field):
-    widget = forms.Textarea
-
-    def to_python(self, value):
-        if not value:
-            return []
-        return list(val for val in re.split("[\s;,]+", value) if val)
-
 class MultiInvitationForm(forms.Form):
-    recipients = ListField(
+    recipients = StudentUsernamesField(
         label="Usernames",
         required=True,
         help_text="Enter one or more usernames separated by commas. Usernames are case sensitive."
     )
-
-    def clean_recipients(self):
-        recipients = self.cleaned_data['recipients']
-        existing = User.objects.filter(
-            username__in=recipients,
-            profile__is_student=True
-        ).values_list('username', flat=True)
-        nonexisting = list(set(recipients) - set(existing))
-        if nonexisting:
-            raise forms.ValidationError(
-                "Found no users named: %(users)s",
-                code='nonexistant-user',
-                params={'users':', '.join(nonexisting)}
-            )
-        return recipients
--- profiles/tests/test_parent.py
+++ profiles/tests/test_parent.py
@@ -1,5 +1,6 @@
 import pytest
 import mock
+from datetime import datetime
 from profiles import forms, models
 from django.contrib.auth.models import User
 
@@ -98,3 +99,42 @@ def test_form_ignores_unspecified_profile_fields():
     assert not user.profile.is_student
     assert user.profile.title == ''
 
+def test_connect_form_requires_usernames():
+    form = forms.parent.ConnectForm(data={})
+    assert not form.is_valid()
+    assert "usernames" in form.errors.as_data()
+    assert form.errors.as_data()["usernames"][0].code == 'required'
+
+@pytest.mark.django_db
+def test_connect_form_validates_existance():
+    form = forms.parent.ConnectForm(data={
+        "usernames": "nope"
+    })
+    assert not form.is_valid()
+    assert "usernames" in form.errors.as_data()
+    assert form.errors.as_data()["usernames"][0].code == 'nonexistant-user'
+
+@pytest.mark.django_db
+def test_connect_form_connects_to_usernames():
+    parent = User(username="parent")
+    parent_profile = models.Profile(is_parent=True)
+    parent_profile.user = parent
+    parent.save()
+    parent_profile.user = parent
+    parent_profile.save()
+
+    child = User(username="child")
+    child_profile = models.Profile(is_student=True, birthday=datetime.now())
+    child_profile.user = child
+    child.save()
+    child_profile.user = child
+    child_profile.save()
+
+    form = forms.parent.ConnectForm(instance=parent.profile, data={
+        "usernames": "child"
+    })
+    assert form.is_valid()
+    assert models.ParentConnection.objects.all().count() == 0
+    form.save()
+    assert models.ParentConnection.objects.all().count() == 1
+    assert [profile.id for profile in parent.profile.child_profiles.all()] == [child.profile.id]
