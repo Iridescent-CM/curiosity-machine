6136ae57d401ff887d06041419fc95f8e2e87000
Mike Larsson <mikelarssonftw@gmail.com>
2015-07-16 15:02:12 -0400

    Send modal show/hide and form submission events to GA

---
+++ curiositymachine/static/js/ga-events.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $('.modal').on('show.bs.modal', function(){
+    ga('send', 'event', 'modal', 'show', $(this).attr('id'));
+  });
+  $('.modal').on('hide.bs.modal', function(){
+    ga('send', 'event', 'modal', 'hide', $(this).attr('id'));
+  });
+
+  $('form').on('submit', function(){
+    ga('send', 'event', 'form', 'submit', $(this).attr('action'));
+  });
+
+});
