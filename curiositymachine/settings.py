58c4a3578d6a006b6eb3a18cbfc1bda349255605
Mike Larsson <mikelarssonftw@gmail.com>
2015-10-07 17:00:51 -0400

    Compressing css and js, adding GA snippet to new base

--- curiositymachine/settings.py
+++ curiositymachine/settings.py
@@ -157,6 +157,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = 'staticfiles'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "curiositymachine/sass/vendors/bootstrap/dist/"),
+)
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
