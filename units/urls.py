24791f19e8c6422de667d5e39f3b86f020458340
Mike Larsson <mikelarssonftw@gmail.com>
2015-10-15 10:30:16 -0400

    Remove units pages from public whitelist

--- units/urls.py
+++ units/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, url
 from units import views
-from curiositymachine.decorators import whitelist
-
-public = whitelist('public')
 
 urlpatterns = patterns('units.views',
-    url(r'^$', public(views.units), name='units'),
-    url(r'^(?P<unit_id>\d+)/$', public(views.unit), name='unit'),
+    url(r'^$', views.units, name='units'),
+    url(r'^(?P<unit_id>\d+)/$', views.unit, name='unit'),
 )
