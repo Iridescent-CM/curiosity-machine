686f982edbd54b7a0caed26bb18df99ed71ea278
Mike Larsson <mikelarssonftw@gmail.com>
2015-11-30 14:35:04 -0500

    Add copy_template management command

---
+++ cmemails/management/commands/copy_template.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from optparse import make_option
+import mandrill
+
+class Command(BaseCommand):
+    help = 'Copy Mandrill template'
+    args = '<source name> <target name>'
+    option_list = BaseCommand.option_list + (
+        make_option(
+            "-p", "--publish",
+            action="store_true",
+            dest="publish",
+            default=False,
+            help="Publish target template, instead of saving changes as draft"
+        ),
+    )
+
+    def handle(self, *args, **options):
+        if len(args) != 2:
+            raise CommandError('Provide source and target template names')
+
+        (source_name, target_name) = args
+
+        mandrill_client = mandrill.Mandrill(settings.MANDRILL_API_KEY)
+        try:
+            source_info = mandrill_client.templates.info(name=source_name)
+        except mandrill.UnknownTemplateError:
+            raise CommandError('No template named %s through Mandrill API key %s' % (source_name, settings.MANDRILL_API_KEY))
+
+        copy_info = {
+            "from_email": source_info['publish_from_email'],
+            "from_name": source_info['publish_from_name'],
+            "subject": source_info['publish_subject'],
+            "code": source_info['publish_code'],
+            "text": source_info['publish_text'],
+            "publish": options["publish"],
+            "labels": source_info['labels']
+        }
+
+        try:
+            mandrill_client.templates.update(name=target_name, **copy_info)
+        except mandrill.UnknownTemplateError:
+            mandrill_client.templates.add(name=target_name, **copy_info)
+
