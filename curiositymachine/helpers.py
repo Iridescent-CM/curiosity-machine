11d57524cab387af3be43af0b27f2dd3cd055d2d
Mike Larsson <mikelarssonftw@gmail.com>
2015-11-30 13:24:26 -0500

    Remove l from group code letters because it looks like I in some fonts

--- curiositymachine/helpers.py
+++ curiositymachine/helpers.py
@@ -2,7 +2,8 @@ import string
 import random
 
 # no vowels to avoid accidental word generation
-CHARS = list('bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ0123456789')
+# no l to avoid I/l confusion
+CHARS = list('bcdfghjkmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ0123456789')
 
 def random_string(length=5):
     return "".join(random.choice(CHARS) for _ in range(length))
