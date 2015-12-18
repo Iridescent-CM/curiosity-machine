90c932c9cf0b982db916bdc63465d9f126ed7836
Mike Larsson <mikelarssonftw@gmail.com>
2015-12-18 15:35:36 -0500

    Oops; fix cyclical call to subscribe

--- cmemails/__init__.py
+++ cmemails/__init__.py
@@ -4,10 +4,10 @@ from .mailer import deliver_email # deprecated
 
 import django_rq
 from .mandrill import send_template
-from .mailchimp import subscribe
+from .mailchimp import subscribe as _subscribe
 
 def send(**kwargs):
     django_rq.enqueue(send_template, **kwargs)
 
 def subscribe(*args, **kwargs):
-    django_rq.enqueue(subscribe, *args, **kwargs)
+    django_rq.enqueue(_subscribe, *args, **kwargs)
