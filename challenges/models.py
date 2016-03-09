f7914850966ed2dd5ac8a5f2216a0254d1c3ea1d
Mike Larsson <mikelarssonftw@gmail.com>
2016-03-09 10:43:43 -0500

    Fix #784; remove unused example fields

--- challenges/models.py
+++ challenges/models.py
@@ -239,11 +239,6 @@ class Example(models.Model): # media that a mentor has selected to be featured o
     created_at = models.DateTimeField(auto_now_add=True, default=now)
     updated_at = models.DateTimeField(auto_now=True, default=now)
 
-    # DEPRECATED
-    # TODO: remove the following fields; the inspiration gallery currently expects only images that are attached to a progress
-    _name = models.TextField(blank=True, verbose_name="name", db_column="name", help_text="The student's username in plain text. This can be left blank if a progress is set, in which case the progress's student username will be automatically used instead.")
-    video = models.ForeignKey(Video, null=True, blank=True, on_delete=models.SET_NULL, help_text="Each example must have an image or a video, or both, to be displayed correctly.")
-
     objects = ExampleQuerySet().as_manager()
 
     @property
