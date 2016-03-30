2ebc2bf1f1cf06764593171bbff5ea6823bae187
Mike Larsson <mikelarssonftw@gmail.com>
2016-03-30 14:55:48 -0400

    basic EDP mentor and student commenting

--- cmcomments/forms.py
+++ cmcomments/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from curiositymachine.forms import FilePickerURLField
+from curiositymachine.forms import FilePickerMediaField
 
 class CommentForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea, required=False)
-    video_filepicker_url = FilePickerURLField(mimetypes="video/*", openTo='VIDEO', services='VIDEO,COMPUTER', required=False)
-    picture_filepicker_url = FilePickerURLField(mimetypes="image/*", openTo='WEBCAM', services='WEBCAM,COMPUTER', required=False)
+    visual_media = FilePickerMediaField(
+        mimetypes="video/*,image/*",
+        openTo='WEBCAM',
+        services='VIDEO,WEBCAM,COMPUTER',
+        required=False
+    )
     question_text = forms.CharField(widget=forms.HiddenInput, required=False)
