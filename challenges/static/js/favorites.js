bfd86a965acbd2bf1cb96ff252c769e6fe23effc
Mike Larsson <mikelarssonftw@gmail.com>
2016-04-26 11:08:40 -0400

    Remove junk from favorites.js

--- challenges/static/js/favorites.js
+++ challenges/static/js/favorites.js
@@ -6,32 +6,3 @@ $(function(){
     });
   });
 });
-
-function setupFavoriteChallengesTab() {
-	$('a[data-toggle="tabajax"]').on('show.bs.tab', function (e) {
-		var $this = $(e.target),
-				loadurl = $this.attr('href'),
-				targ = $this.attr('data-target');
-		$(targ).html("");
-	});
-	$('a[data-toggle="tabajax"]').on('shown.bs.tab', function (e) {
-		var $this = $(e.target),
-				loadurl = $this.attr('href'),
-				targ = $this.attr('data-target');
-
-		$.get(loadurl, function(data) {
-				$(targ).html(data);
-				$(targ).children().owlCarousel({
-					items : 4,
-					itemsCustom : false,
-					itemsDesktop : [1199,3],
-					itemsDesktopSmall : [980,3],
-					itemsTablet: [768,2],
-					itemsTabletSmall: false,
-					itemsMobile : [479,1],
-					singleItem : false,
-					itemsScaleUp : false
-			});
-		});
-	});
-}
