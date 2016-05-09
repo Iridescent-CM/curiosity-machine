d57eab21251ee8ed41ea018909bb7963c76fde69
Mike Larsson <mikelarssonftw@gmail.com>
2016-05-09 09:56:40 -0400

    Register Member in admin, add student and educator limit fields to Membership

--- memberships/admin.py
+++ memberships/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from memberships.models import Membership
+from memberships.models import Membership, Member
 
-admin.site.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'expiration')
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('id', 'membership', 'user')
+
+admin.site.register(Membership, MembershipAdmin)
+admin.site.register(Member, MemberAdmin)
--- memberships/models.py
+++ memberships/models.py
@@ -6,9 +6,14 @@ class Membership(models.Model):
     name = models.CharField(unique=True, max_length=255, null=False, blank=False)
     expiration = models.DateField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
+    student_limit = models.PositiveIntegerField(null=True, blank=True)
+    educator_limit = models.PositiveIntegerField(null=True, blank=True)
 
     challenges = models.ManyToManyField(Challenge, null=True, blank=True)
     members = models.ManyToManyField(User, through='Member', through_fields=('membership', 'user'), null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Member(models.Model):
     membership = models.ForeignKey(Membership)
