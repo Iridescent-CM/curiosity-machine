43cde1764c1c7d1476dd0347acf00d6cc1c9dff4
Mike Larsson <mikelarssonftw@gmail.com>
2016-05-31 17:33:15 -0400

    Add import_members admin url

--- memberships/admin/__init__.py
+++ memberships/admin/__init__.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.http import HttpResponse
+from django.conf.urls import url
+from django.shortcuts import get_object_or_404
 from memberships.models import Membership, Member, MemberLimit
 
 class MemberLimitInline(admin.TabularInline):
@@ -12,6 +15,17 @@ class MembershipAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     inlines = [MemberLimitInline]
     filter_horizontal = ['challenges']
+
+    def get_urls(self):
+        urls = super(MembershipAdmin, self).get_urls()
+        extra_urls = [
+            url(r'^(?P<id>\d+)/import_members/$', self.admin_site.admin_view(self.import_members), name="import_members"),
+        ]
+        return extra_urls + urls
+
+    def import_members(self, request, *args, **kwargs):
+        obj = get_object_or_404(Membership, id=kwargs.get('id'))
+        return HttpResponse('OK')
 
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('id', 'membership', 'user')
