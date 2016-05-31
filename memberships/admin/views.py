a431276e52d3277643f4876d774f2c1ca287f07f
Mike Larsson <mikelarssonftw@gmail.com>
2016-05-31 18:07:26 -0400

    fetch membership object and put it in template context

--- memberships/admin/views.py
+++ memberships/admin/views.py
@@ -1,4 +1,15 @@
 from django.views.generic.base import TemplateView
+from django.shortcuts import get_object_or_404
+from memberships.models import Membership
 
 class ImportView(TemplateView):
     template_name = "memberships/admin/import_members/form.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.membership = get_object_or_404(Membership, id=kwargs.get('id'))
+        return super(ImportView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(ImportView, self).get_context_data(**kwargs)
+        context['original'] = self.membership
+        return context
