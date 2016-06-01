2e263b4006a4d41ae18acac388e4cde94d4c6851
Mike Larsson <mikelarssonftw@gmail.com>
2016-06-01 16:05:45 -0400

    refactor out RequiredObjectMixin

--- memberships/admin/views.py
+++ memberships/admin/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
+from django.views.generic.detail import SingleObjectMixin
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib import admin
@@ -15,13 +16,39 @@ class ExtraContextMixin(object):
         context.update(self.extra_context)
         return context
 
-class ImportView(ExtraContextMixin, FormView):
+class RequiredObjectMixin(SingleObjectMixin):
+    object = None # SingleObjectMixin checks self.object
+    attribute_name = None
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Find the required object and put it in its attribute
+        """
+        setattr(self, self.attribute_name, self.get_object())
+        return super(RequiredObjectMixin, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        """
+        Insert the single object into the context dict.
+        Rewrite of SingleObjectMixin.get_context_data to support dynamic attribute name.
+        """
+        context = {}
+        if getattr(self, self.attribute_name):
+            context[self.attribute_name] = getattr(self, self.attribute_name)
+            context_object_name = self.get_context_object_name(getattr(self, self.attribute_name))
+            if context_object_name:
+                context[context_object_name] = getattr(self, self.attribute_name)
+        context.update(kwargs)
+        return super(RequiredObjectMixin, self).get_context_data(**context)
+
+class ImportView(ExtraContextMixin, RequiredObjectMixin, FormView):
     template_name = "memberships/admin/import_members/form.html"
     form_class = ImportForm
 
-    def dispatch(self, request, *args, **kwargs):
-        self.membership = get_object_or_404(Membership, id=kwargs.get('id'))
-        return super(ImportView, self).dispatch(request, *args, **kwargs)
+    model = Membership
+    pk_url_kwarg = "id"
+    context_object_name = "original"
+    attribute_name = "membership"
 
     def get_context_data(self, **kwargs):
         context = super(ImportView, self).get_context_data(**kwargs)
@@ -31,7 +58,6 @@ class ImportView(ExtraContextMixin, FormView):
         adminForm = admin.helpers.AdminForm(form, fieldsets, {})
 
         context.update(
-            original = self.membership,
             adminform = adminForm
         )
         return context
@@ -42,14 +68,10 @@ class ImportView(ExtraContextMixin, FormView):
         })
         return HttpResponseRedirect("%s?%s=%s" % (url, "csv", form.cleaned_data["csv_file"]))
 
-class ProcessView(ExtraContextMixin, TemplateView):
+class ProcessView(ExtraContextMixin, RequiredObjectMixin, TemplateView):
     template_name = "memberships/admin/import_members/process.html"
 
-    def dispatch(self, request, *args, **kwargs):
-        self.membership = get_object_or_404(Membership, id=kwargs.get('id'))
-        return super(ProcessView, self).dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super(ProcessView, self).get_context_data(**kwargs)
-        context.update(original = self.membership)
-        return context
+    model = Membership
+    pk_url_kwarg = "id"
+    context_object_name = "original"
+    attribute_name = "membership"
