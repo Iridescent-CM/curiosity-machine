57b9e5a4b18cc6db2179990b4e2fcb1417046615
Mike Larsson <mikelarssonftw@gmail.com>
2016-06-13 12:04:07 -0400

    add s3 member import integration test

--- memberships/tests/test_member_import_model.py
+++ memberships/tests/test_member_import_model.py
@@ -1,4 +1,5 @@
 import pytest
+from mock import patch
 
 from memberships.factories import MembershipFactory
 
@@ -6,6 +7,7 @@ from memberships.models import MemberImport
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django_rq import get_worker, get_queue
+from django_s3_storage.storage import S3Storage
 
 @pytest.mark.django_db
 def test_saving_a_new_member_import_processes_the_input_file_in_a_worker():
@@ -19,6 +21,21 @@ def test_saving_a_new_member_import_processes_the_input_file_in_a_worker():
     assert queue.count == 1
     assert not MemberImport.objects.all().first().output.name
 
-    get_worker().work(burst=True)  
+    get_worker().work(burst=True)
 
     assert MemberImport.objects.all().first().output.name
+
+@pytest.mark.django_db
+def test_member_import_lifecycle_with_s3():
+    with patch("django.core.files.storage.default_storage._wrapped", S3Storage()) as storage:
+        queue = get_queue()
+        queue.empty()
+
+        membership = MembershipFactory()
+        member_import = MemberImport(input=SimpleUploadedFile("file.csv", b'file contents'), membership=membership)
+        member_import.save()
+        get_worker().work(burst=True)
+
+        result = MemberImport.objects.all().first()
+        assert storage.exists(result.output.name)
+        assert storage.exists(result.input.name)
