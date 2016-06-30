9829fbdb751fad89529a321a53e6dca7123c2fee
Mike Larsson <mikelarssonftw@gmail.com>
2016-06-30 12:04:06 -0400

    educator dashboard context test

--- memberships/factories.py
+++ memberships/factories.py
@@ -1,8 +1,17 @@
 import factory
 import factory.django
+from factory import post_generation
 
 from . import models
 
 class MembershipFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Membership
+
+    name = factory.fuzzy.FuzzyText(prefix="membership-")
+
+    @post_generation
+    def members(obj, create, extracted, **kwargs):
+        if extracted:
+            for user in extracted:
+                models.Member.objects.create(membership=obj, user=user)
