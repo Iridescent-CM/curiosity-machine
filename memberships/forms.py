62fbb97a9c62876f7b645e9b189a69a588803845
Mike Larsson <mikelarssonftw@gmail.com>
2016-07-21 17:51:45 -0400

    Add email to csv user form

--- memberships/forms.py
+++ memberships/forms.py
@@ -13,7 +13,7 @@ class RowUserForm(forms.ModelForm):
     """
     class Meta:
         model = User
-        fields = ['username', 'password', 'first_name', 'last_name']
+        fields = ['username', 'password', 'first_name', 'last_name', 'email']
 
     def save(self, commit=True):
         user = super().save(commit=False)
