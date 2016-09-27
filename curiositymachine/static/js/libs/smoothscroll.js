9475f1afd1bcd0de8414be66ad02c4e6d712c5a2
Savannah Million <savannahmillion@gmail.com>
2016-09-27 15:08:37 -0500

    fixes smoothscroll js from overlapping with pagination js

--- curiositymachine/static/js/libs/smoothscroll.js
+++ curiositymachine/static/js/libs/smoothscroll.js
@@ -1,14 +1,12 @@
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+  $('a[data-smoothscroll]').click(function() {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
     }
   });
 });
