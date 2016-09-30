2781271c41f2fe0b0533c8c9b294599a47fdc70b
Mike Larsson <mikelarssonftw@gmail.com>
2016-09-30 14:28:32 -0400

    some more dependency removal, fully remove training app

--- training/factories.py
+++
@@ -1,8 +0,0 @@
-import factory
-import factory.django
-
-from . import models
-
-class TaskFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = models.Task
--- training/templatetags/is_greater_length.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def is_greater_length(subject, length):
-	return len(subject) > length
