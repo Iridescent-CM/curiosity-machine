950b76e8bd4f6519366a18eaa54b6dffe75f3f21
Mike Larsson <mikelarssonftw@gmail.com>
2016-10-13 15:39:35 -0400

    Add command help text and -l option

--- curiositymachine/management/commands/fixtures.py
+++ curiositymachine/management/commands/fixtures.py
@@ -7,15 +7,27 @@ def is_module_function(mod, func):
 
 class Command(BaseCommand):
 
+    help = 'Run fixture functions for data setup'
+
     def add_arguments(self, parser):
         parser.add_argument('fixture', nargs='*', type=str)
 
+        parser.add_argument('-l', '--list',
+            action='store_true',
+            dest='list',
+            default=False,
+            help='List available fixture functions'
+        )
+
+    def list_available(self):
+        available = [func.__name__ for func in _fixtures.__dict__.values() if is_module_function(_fixtures, func)]
+        self.stdout.write("Available fixtures:")
+        for item in available:
+            self.stdout.write("\t%s" % item)
+
     def handle(self, *args, **options):
-        if not options['fixture']:
-            available = [func.__name__ for func in _fixtures.__dict__.values() if is_module_function(_fixtures, func)] 
-            self.stdout.write("Available fixtures:")
-            for item in available:
-                self.stdout.write("\t%s" % item)
+        if not options['fixture'] or options['list']:
+            self.list_available()
             return
 
         for name in options['fixture']:
