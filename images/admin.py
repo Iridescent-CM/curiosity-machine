858be6d98428489c9a69df9c524d4979ae4f0c6b
Mike Larsson <mikelarssonftw@gmail.com>
2017-08-03 16:22:01 -0400

    Add more info to image and video admin list displays and edit pages

--- images/admin.py
+++ images/admin.py
@@ -16,7 +16,17 @@ class ImageAdminForm(forms.ModelForm):
         }
 
 class ImageAdmin(admin.ModelAdmin):
-    fields = ('source_url',)
+    fields = ('source_url', 'url_link')
+    readonly_fields = ('url_link',)
+    list_display = ('id', 'url_link', 'source_url_link')
+
+    def url_link(self, obj):
+        return '<a href="%s">%s</a>' % (obj.url, obj.url)
+    url_link.allow_tags = True
+
+    def source_url_link(self, obj):
+        return '<a href="%s">%s</a>' % (obj.source_url, obj.source_url)
+    source_url_link.allow_tags = True
 
     def get_form(self, request, obj=None, **kwargs):
         if obj is None:
--- videos/admin.py
+++ videos/admin.py
@@ -3,6 +3,7 @@ from videos.models import Video, EncodedVideo
 from django import forms
 from curiositymachine.widgets import FilePickerVideoPickWidget
 from django.conf import settings
+import json
 
 class VideoAdminForm(forms.ModelForm):
     class Meta:
@@ -16,7 +17,30 @@ class VideoAdminForm(forms.ModelForm):
         }
 
 class VideoAdmin(admin.ModelAdmin):
-    fields = ('source_url',)
+    fieldsets = (
+        (None, {
+            'fields': ('source_url', 'url_link')
+        }),
+        ('Debug info', {
+            'fields': ('job_details',),
+            'classes': ('collapse',)
+        }),
+    )
+    readonly_fields = ('url_link', 'job_details')
+    list_display = ('id', 'url_link', 'source_url_link')
+
+    def url_link(self, obj):
+        return '<a href="%s">%s</a>' % (obj.url, obj.url)
+    url_link.allow_tags = True
+
+    def source_url_link(self, obj):
+        return '<a href="%s">%s</a>' % (obj.source_url, obj.source_url)
+    source_url_link.allow_tags = True
+
+    def job_details(self, obj):
+        return '<pre><code>\n%s</code></pre>' % (json.dumps(json.loads(obj.raw_job_details), indent=4, sort_keys=True))
+    job_details.allow_tags = True
+
     def get_form(self, request, obj=None, **kwargs):
         if obj is None:
             return VideoAdminForm
