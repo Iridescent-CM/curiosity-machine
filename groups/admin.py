7f5fa284b6767b9e70e65e7d9500be587f63087a
Mike Larsson <mikelarssonftw@gmail.com>
2017-08-15 13:35:11 -0400

    Remove groups app completely

--- groups/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# This app is deprecated but not ready for full removal. This removes it from the
-# admin, and the whole app should go away before long.
-#
-#admin.site.register(models.Group, GroupAdmin)
-#admin.site.register(models.Invitation, InvitationAdmin)
-#admin.site.register(models.Membership, MembershipAdmin)
