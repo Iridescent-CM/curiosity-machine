f1295108e2f0e63baf6e1bf9b168583e94be5b01
Mike Larsson <mikelarssonftw@gmail.com>
2017-10-25 17:41:43 +0100

    #1331 chicago family science signups (#1333)
    * add profiles management command to assign users with source to membership
    
    * add family science school names as source options
    
    * make source to membership command handle multiple source to membership specifications at once

---
+++ profiles/management/commands/source_to_membership.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand, CommandError
+from memberships.models import Membership, Member
+from profiles.models import Profile
+import logging
+
+logger = logging.getLogger(__name__)
+
+def add_all(source, membership_id):
+    membership = Membership.objects.get(pk=membership_id)
+    profiles = Profile.objects.filter(source=source).exclude(user__membership=membership)
+    for profile in profiles:
+        user = profile.user
+        Member.objects.create(membership=membership, user=user)
+        logger.info("Added %s to %s" % (user, membership))
+
+class Command(BaseCommand):
+    help = 'Add accounts with sources to memberships'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "specifiers",
+            action="store",
+            nargs="+",
+            help="source:membership_id pairs"
+        )
+
+    def handle(self, *args, **options):
+        for specifier in options['specifiers']:
+            try:
+                (source, membership_id) = specifier.split(':')
+                add_all(source, membership_id)
+            except ValueError:
+                raise CommandError(
+                    ("Unable to parse specifier %s. Specifiers should be " +
+                    "the source and membership id joined by a colon.") % specifier
+                )
