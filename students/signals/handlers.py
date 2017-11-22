98c735f29c268253e0b287940796c26c8ab0b162
Mike Larsson <mikelarssonftw@gmail.com>
2017-11-22 12:14:29 -0500

    Moar QA fixes (#1384)
    * fix small signal TODO
    
    * fix mentor public view

---
+++ students/signals/handlers.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from students.models import StudentProfile
+
+@receiver(post_save, sender=StudentProfile)
+def auto_approve_non_coppa_students(sender, instance, created, **kwargs):
+    if created and not kwargs.get('raw'):
+        if not instance.is_underage():
+            instance.user.extra.approved = True
+            instance.user.extra.save(update_fields=['approved'])
