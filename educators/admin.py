2224fce7c6c380dbc7fddbf3fad3ab0c1426e6d4
Mike Larsson <mikelarssonftw@gmail.com>
2017-11-29 14:35:23 -0500

    remove admin for profile models that have moved to other apps, add admin there

--- educators/admin.py
+++ educators/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ImpactSurveyAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'student_count',
+        'teacher_count',
+        'challenge_count',
+        'hours_per_challenge',
+        'in_classroom',
+        'out_of_classroom',
+        'created_at',
+    )
+    raw_id_fields = ('user',)
+    readonly_fields = (
+        'id',
+        'user',
+        'student_count',
+        'teacher_count',
+        'challenge_count',
+        'hours_per_challenge',
+        'in_classroom',
+        'out_of_classroom',
+        'comment',
+        'created_at',
+    )
+
+admin.site.register(ImpactSurvey, ImpactSurveyAdmin)
