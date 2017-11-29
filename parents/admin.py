5c356e27517dad1e58db376c478ef7db76492b74
Mike Larsson <mikelarssonftw@gmail.com>
2017-11-29 16:41:16 -0500

    #1362 fix for impact surveys and parent connections (#1419)
    * remove admin for profile models that have moved to other apps, add admin there
    
    * migrate parent connections from profiles to parents
    
    * remove duplicated migration

--- parents/admin.py
+++ parents/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ParentConnectionAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'parent', 'parent_email', 'child', 'child_email', 'active', 'removed']
+    list_filter = ['active', 'removed']
+    search_fields = [
+        'parent_profile__user__username',
+        'child_profile__user__username',
+        'parent_profile__user__email',
+        'child_profile__user__email'
+    ]
+
+    def parent(self, obj):
+        return obj.parent_profile.user.username
+    parent.admin_order_field = 'parent_profile__user__username'
+
+    def parent_email(self, obj):
+        return obj.parent_profile.user.email
+    parent.admin_order_field = 'parent_profile__user__email'
+
+    def child(self, obj):
+        return obj.child_profile.user.username
+    child.admin_order_field = 'child_profile__user__username'
+
+    def child_email(self, obj):
+        return obj.child_profile.user.email
+    child.admin_order_field = 'child_profile__user__email'
+
+admin.site.register(ParentConnection, ParentConnectionAdmin)
