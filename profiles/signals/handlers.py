b40809301876291a33bb75b0785bbfe82bfa1609
Mike Larsson <mikelarssonftw@gmail.com>
2017-11-30 11:27:23 -0500

    add new created_profile signal and use it for welcome emails

--- profiles/signals/handlers.py
+++ profiles/signals/handlers.py
@@ -1,16 +1,20 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from profiles.models import UserExtra, User
+from profiles.models import UserExtra, User, BaseProfile
 from curiositymachine import signals
 
-# attach to User model and our proxy
 @receiver(post_save, sender=get_user_model())
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_extra(sender, instance, created, **kwargs):
     if kwargs.get('raw'):
         return
     if created:
         if not hasattr(instance, "extra"):
             UserExtra.objects.create(user=instance)
         signals.created_account.send(sender=instance)
+
+@receiver(post_save)
+def create_profile(sender, instance, created, **kwargs):
+    if issubclass(sender, BaseProfile) and created:
+        signals.created_profile.send(sender=instance.user)
