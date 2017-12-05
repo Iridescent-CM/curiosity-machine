0d80f1e0f83f8be65f6efd9469d244b598a96e7a
Mike Larsson <mikelarssonftw@gmail.com>
2017-12-05 12:27:00 -0500

    novalidate forms for better required field indication

--- curiositymachine/templatetags/bootstrap4.py
+++ curiositymachine/templatetags/bootstrap4.py
@@ -7,7 +7,7 @@ def form_group(field, **kwargs):
     context = {
         "field": field,
         "field_class": "form-control",
-        "help_text_classes": kwargs.get("help_text_classes", "text-muted")
+        "help_text_classes": kwargs.get("help_text_classes", "text-muted"),
     }
     context.update(kwargs)
     return context
