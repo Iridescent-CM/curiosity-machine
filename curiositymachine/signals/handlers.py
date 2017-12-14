b0abe8f4b3dab302c9473b4d57b8ec7b747c228e
Mike Larsson <mikelarssonftw@gmail.com>
2017-12-14 11:13:12 -0500

    fix comment notification recipients, with tests (#1481)

--- curiositymachine/signals/handlers.py
+++ curiositymachine/signals/handlers.py
@@ -2,15 +2,18 @@ from curiositymachine import signals
 from django.dispatch import receiver
 from notifications.signals import notify
 
+def recipients_for(comment, sender):
+    progress = comment.challenge_progress
+    if sender != progress.student:
+        return [progress.student]
+    elif progress.mentor:
+        return [progress.mentor]
+    else:
+        return []
+
 @receiver(signals.posted_comment)
 def create_comment_notification(sender, **kwargs):
     comment = kwargs['comment']
     progress = comment.challenge_progress
-
-    recipients = [progress.student]
-    if progress.mentor:
-        recipients.append(progress.mentor)
-    recipients.remove(sender)
-
-    for recipient in recipients:
+    for recipient in recipients_for(comment, sender):
         notify.send(sender, recipient=recipient, verb="posted", action_object=comment, target=progress)
