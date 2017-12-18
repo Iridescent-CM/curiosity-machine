125576f302822c1eedf9cdcd69fdbc25594696a2
Mike Larsson <mikelarssonftw@gmail.com>
2017-12-18 13:04:51 -0500

    only send project started signal if commenter is progress owner/student (#1495)

---
+++ curiositymachine/tests/signals/test_started_first_project.py
@@ -0,0 +1,38 @@
+import mock
+import pytest
+from challenges.factories import *
+from mentors.factories import *
+from profiles.factories import *
+from ... import signals
+
+@pytest.mark.django_db
+def test_called_for_project_owner():
+    handler = mock.MagicMock()
+    signals.started_first_project.connect(handler)
+
+    user = UserFactory()
+    first_progress = ProgressFactory(student=user)
+
+    assert not handler.called
+
+    first_progress.comments.create(user=user, text="comment", stage=1)
+
+    handler.assert_called_once_with(signal=signals.started_first_project, progress=first_progress, sender=user)
+    handler.reset_mock()
+
+    second_progress = ProgressFactory(student=user)
+    second_progress.comments.create(user=user, text="comment", stage=1)
+
+    assert not handler.called
+
+@pytest.mark.django_db
+def test_not_called_for_project_mentor():
+    handler = mock.MagicMock()
+    signals.started_first_project.connect(handler)
+
+    user = MentorFactory()
+    first_progress = ProgressFactory(mentor=user)
+
+    first_progress.comments.create(user=user, text="comment", stage=1)
+
+    assert not handler.called
