e900026cd462ee9455efaf53a3c6077bdfc0c80f
Mike Larsson <mikelarssonftw@gmail.com>
2018-01-11 17:46:42 -0500

    stage tabs in family dashboard

--- families/urls.py
+++ families/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^profile/new/$', create, name="create_profile"),
     url(r'^profile/edit/$', edit, name="edit_profile"),
     url(r'^home/$', home, name="home"),
+    url(r'^stage-1/$', stage_1, name="stage_1"),
+    url(r'^stage-2/$', stage_2, name="stage_2"),
 ]
--- families/views.py
+++ families/views.py
@@ -1,3 +1,5 @@
+from challenges.models import Challenge
+from django.conf import settings
 from django.contrib import messages
 from django.urls import reverse
 from django.utils.functional import lazy
@@ -5,6 +7,7 @@ from django.views.generic import CreateView, TemplateView, UpdateView
 from profiles.decorators import only_for_role
 from profiles.models import UserRole
 from profiles.views import EditProfileMixin
+from units.models import Unit
 from .forms import *
 from .models import *
 
@@ -35,3 +38,18 @@ class HomeView(TemplateView):
     template_name = "families/home.html"
 
 home = only_for_family(HomeView.as_view())
+
+class StageView(TemplateView):
+    stage = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.config = settings.AICHALLENGE_STAGES[self.stage]
+
+    def get_context_data(self, **kwargs):
+        kwargs["challenges"] = Challenge.objects.filter(id__in=self.config['challenges'])
+        kwargs["units"] = Unit.objects.filter(id__in=self.config['units'])
+        return super().get_context_data(**kwargs) 
+
+stage_1 = only_for_family(StageView.as_view(template_name="families/stages/stage_1.html", stage=1))
+stage_2 = only_for_family(StageView.as_view(template_name="families/stages/stage_2.html", stage=2))
