2e1c58245314c3e8b7100ce18f5f46e56b9d91b9
Mike Larsson <mikelarssonftw@gmail.com>
2018-01-23 17:59:33 -0500

    don't send hellosign if profile is already approved (#1553)

--- hellosign/signals/handlers.py
+++ hellosign/signals/handlers.py
@@ -5,9 +5,9 @@ from django.dispatch import receiver
 
 @receiver(signals.created_profile)
 def send_consent_email(sender, **kwargs):
-    if sender.extra.send_welcome:
-        if sender.extra.is_student:
-            if sender.studentprofile.is_underage():
-                # For underage users we will be using
-                # the Hellosign e-ign option, not email.
-                send_underage_consent_form(sender)
+    if (sender.extra.send_welcome
+        and sender.extra.is_student
+        and sender.studentprofile.is_underage()
+        and not sender.extra.approved
+    ):
+        send_underage_consent_form(sender)
