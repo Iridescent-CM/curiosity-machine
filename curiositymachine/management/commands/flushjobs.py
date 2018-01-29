5216225c5972f8c6e8c41283363019d9faaf381d
Mike Larsson <mikelarssonftw@gmail.com>
2018-01-29 12:30:27 -0500

    add flags to be able to flush specific jobs, failed jobs

--- curiositymachine/management/commands/flushjobs.py
+++ curiositymachine/management/commands/flushjobs.py
@@ -13,16 +13,43 @@ class Command(BaseCommand):
             default=False,
             help='Show scheduled jobs without actually flushing them'
         )
+        parser.add_argument(
+            '--failed',
+            action='store_true',
+            dest='failed',
+            default=False,
+            help='Use failed queue, not default queue'
+        )
+        parser.add_argument(
+            '--id',
+            action='append',
+            dest='ids',
+            default=[],
+            help='ID(s) of job(s) to remove, if not flushing all'
+        )
 
     def handle(self, *args, **options):
-        queue = django_rq.get_queue()
-        jobs = queue.get_jobs()
+        queue = None
+        jobs = []
+
+        if options['failed']:
+            queue = django_rq.get_failed_queue()
+        else:
+            queue = django_rq.get_queue()
+
+        if options['ids']:
+            for id in options['ids']:
+                jobs.append(queue.fetch_job(id))
+        else:
+            jobs = queue.get_jobs()
+
         for job in jobs:
             self.stdout.write('%s\n-----' % job)
 
         if len(jobs) > 0:
             self.stdout.write('Flushing %d jobs.' % len(jobs))
             if not options['dry-run']:
-                queue.empty()
+                for job in jobs:
+                    queue.remove(job)
         else:
             self.stdout.write('0 jobs scheduled.')
