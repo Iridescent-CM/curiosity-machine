6b9bcbb58b52fda8a5752ad2c668669b39d2e133
Mike Larsson <mikelarssonftw@gmail.com>
2018-02-15 12:40:12 -0500

    refactor some stuff around inactive user checks, add check for family accounts (#1648)

--- cmemails/management/commands/inactive_notifications.py
+++ cmemails/management/commands/inactive_notifications.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from django.utils.timezone import now
 from profiles.models import UserExtra
 from ... import send
 
@@ -6,10 +8,26 @@ class Command(BaseCommand):
     help = 'Send out email notifications for inactive users'
 
     def handle(self, *args, **options):
-        extras = UserExtra.inactive_students()
-        for extra in extras:
-            # TODO: this might be better as a bulk send
+        # TODO: these might be better as a bulk sends
+
+        for extra in (
+            UserExtra.students
+            .inactive_since(settings.EMAIL_INACTIVE_DAYS_STUDENT)
+            .filter(last_inactive_email_sent_on=None)
+        ):
             send(template_name='student-inactive-2-weeks', to=extra.user, merge_vars={
                 'studentname': extra.user.username
             })
-            extra.update_inactive_email_sent_on_and_save()
+            extra.last_inactive_email_sent_on = now()
+            extra.save(update_fields=['last_inactive_email_sent_on'])
+
+        for extra in (
+            UserExtra.families
+            .inactive_since(settings.EMAIL_INACTIVE_DAYS_FAMILY)
+            .filter(last_inactive_email_sent_on=None)
+        ):
+            send(template_name='family-account-inactive-2-weeks', to=extra.user, merge_vars={
+                'username': extra.user.username
+            })
+            extra.last_inactive_email_sent_on = now()
+            extra.save(update_fields=['last_inactive_email_sent_on'])
