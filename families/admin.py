e9d92ac56caecc33100617e4698c761a03bb89e4
Mike Larsson <mikelarssonftw@gmail.com>
2018-02-23 13:57:34 -0500

    don't create empty image objects, warn

--- families/admin.py
+++ families/admin.py
@@ -4,5 +4,6 @@ from .models import *
 class FamilyMemberAdmin(admin.ModelAdmin):
     list_display = ['account', 'first_name', 'last_name', 'family_role']
     search_fields = ['account__username', 'first_name', 'last_name']
+    raw_id_fields = ['image']
 
 admin.site.register(FamilyMember, FamilyMemberAdmin)
