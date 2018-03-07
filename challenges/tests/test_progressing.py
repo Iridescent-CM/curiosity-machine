2422d9a1a858fc0ea7ba942288867e28c084c9c2
Mike Larsson <mikelarssonftw@gmail.com>
2018-03-07 15:15:28 -0500

    add some tests, fix activity link if commenter isn't progress owner

--- challenges/tests/test_progressing.py
+++ challenges/tests/test_progressing.py
@@ -1,4 +1,6 @@
 from cmcomments.factories import *
+from educators.factories import *
+from memberships.factories import *
 from mentors.factories import *
 from students.factories import *
 from ..factories import *
@@ -34,13 +36,22 @@ def test_progress_completed_by_comment():
 
     wrapped_owner = mock.Mock(wraps=NoopActor(owner))
     wrapped_mentor = mock.Mock(wraps=NoopActor(mentor))
+    wrapped_educators = mock.Mock(wraps=NoopActor())
 
-    Progressing(progress=progress, owner=wrapped_owner, mentor=wrapped_mentor).on_comment(comment)
+    Progressing(
+        progress=progress,
+        owner=wrapped_owner,
+        mentor=wrapped_mentor,
+        educators=wrapped_educators,
+    ).on_comment(comment)
+
+    assert wrapped_owner.on_progress_complete.called
+    assert wrapped_mentor.on_progress_complete.called
+    assert wrapped_educators.on_progress_complete.called
 
     assert not wrapped_owner.on_comment_posted.called
     assert not wrapped_mentor.on_comment_received.called
-    assert wrapped_owner.on_progress_complete.called
-    assert wrapped_mentor.on_progress_complete.called
+    assert not wrapped_educators.on_comment_received.called
 
 @pytest.mark.django_db
 def test_owner_comments():
@@ -51,15 +62,19 @@ def test_owner_comments():
 
     wrapped_owner = mock.Mock(wraps=NoopActor(owner))
     wrapped_mentor = mock.Mock(wraps=NoopActor(mentor))
+    wrapped_educators = mock.Mock(wraps=NoopActor())
 
-    Progressing(progress=progress, owner=wrapped_owner, mentor=wrapped_mentor).on_comment(comment)
+    Progressing(
+        progress=progress,
+        owner=wrapped_owner,
+        mentor=wrapped_mentor,
+        educators=wrapped_educators,
+    ).on_comment(comment)
 
-    assert wrapped_owner.on_comment.called
-    assert wrapped_owner.on_comment.call_args[0][0] == progress
-    assert wrapped_owner.on_comment.call_args[0][1] == comment
-    assert wrapped_mentor.on_comment.called
-    assert wrapped_mentor.on_comment.call_args[0][0] == progress
-    assert wrapped_mentor.on_comment.call_args[0][1] == comment
+    for actor in [wrapped_owner, wrapped_mentor, wrapped_educators]:
+        assert actor.on_comment.called
+        assert actor.on_comment.call_args[0][0] == progress
+        assert actor.on_comment.call_args[0][1] == comment
 
 @pytest.mark.django_db
 def test_mentor_comments():
@@ -70,23 +85,26 @@ def test_mentor_comments():
 
     wrapped_owner = mock.Mock(wraps=NoopActor(owner))
     wrapped_mentor = mock.Mock(wraps=NoopActor(mentor))
+    wrapped_educators = mock.Mock(wraps=NoopActor())
 
-    Progressing(progress=progress, owner=wrapped_owner, mentor=wrapped_mentor).on_comment(comment)
+    Progressing(
+        progress=progress,
+        owner=wrapped_owner,
+        mentor=wrapped_mentor,
+        educators=wrapped_educators,
+    ).on_comment(comment)
 
-    assert wrapped_owner.on_comment.called
-    assert wrapped_owner.on_comment.call_args[0][0] == progress
-    assert wrapped_owner.on_comment.call_args[0][1] == comment
-    assert wrapped_mentor.on_comment.called
-    assert wrapped_mentor.on_comment.call_args[0][0] == progress
-    assert wrapped_mentor.on_comment.call_args[0][1] == comment
+    for actor in [wrapped_owner, wrapped_mentor, wrapped_educators]:
+        assert actor.on_comment.called
+        assert actor.on_comment.call_args[0][0] == progress
+        assert actor.on_comment.call_args[0][1] == comment
 
 @pytest.mark.django_db
-def test_base_actor_checks_author():
+def test_base_actor_on_comment_posted():
     owner = StudentFactory()
     mentor = MentorFactory()
     progress = ProgressFactory(owner=owner, mentor=mentor)
     comment = CommentFactory(user=owner, challenge_progress=progress)
-    comment2 = CommentFactory(user=mentor, challenge_progress=progress)
 
     actor = BaseActor(owner)
     actor.on_comment_posted = mock.Mock()
@@ -96,9 +114,64 @@ def test_base_actor_checks_author():
     assert actor.on_comment_posted.called
     assert not actor.on_comment_received.called
 
-    actor.on_comment_posted.reset_mock()
-    actor.on_comment_received.reset_mock()
+@pytest.mark.django_db
+def test_base_actor_on_comment_received():
+    owner = StudentFactory()
+    mentor = MentorFactory()
+    progress = ProgressFactory(owner=owner, mentor=mentor)
+    comment = CommentFactory(user=mentor, challenge_progress=progress)
 
-    actor.on_comment(progress, comment2)
+    actor = BaseActor(owner)
+    actor.on_comment_posted = mock.Mock()
+    actor.on_comment_received = mock.Mock()
+
+    actor.on_comment(progress, comment)
     assert not actor.on_comment_posted.called
     assert actor.on_comment_received.called
+
+@pytest.mark.django_db
+def test_progress_educators_looks_up_connected_educators():
+    student = StudentFactory()
+    progress=ProgressFactory(owner=student)
+    ed = EducatorFactory()
+    MembershipFactory(members=[student, ed], challenges=[progress.challenge])
+    assert [pe.user.id for pe in ProgressEducators(progress).educators] == [ed.id]
+
+@pytest.mark.django_db
+def test_progress_educators_skips_memberships_without_relevant_challenge():
+    student = StudentFactory()
+    progress=ProgressFactory(owner=student)
+    ed = EducatorFactory()
+    MembershipFactory(members=[student, ed], challenges=[])
+    assert not ProgressEducators(progress).educators
+
+@pytest.mark.django_db
+def test_progress_educators_skips_inactive_memberships():
+    student = StudentFactory()
+    progress=ProgressFactory(owner=student)
+    ed = EducatorFactory()
+    MembershipFactory(members=[student, ed], challenges=[], is_active=False)
+    assert not ProgressEducators(progress).educators
+
+@pytest.mark.django_db
+def test_progress_educators_counts_educators_connected_in_multiple_ways_only_once():
+    student = StudentFactory()
+    progress=ProgressFactory(owner=student)
+    ed = EducatorFactory()
+    MembershipFactory(members=[student, ed], challenges=[progress.challenge])
+    MembershipFactory(members=[student, ed], challenges=[progress.challenge])
+    assert [pe.user.id for pe in ProgressEducators(progress).educators] == [ed.id]
+
+@pytest.mark.django_db
+def test_progress_educators_dispatches_to_each_educator():
+    educators = [mock.MagicMock(), mock.MagicMock()]
+    pe = ProgressEducators(mock.Mock(), educators=educators)
+
+    pe.on_progress_complete(mock.Mock())
+    for educator in educators:
+        assert educator.on_progress_complete.called
+
+    pe.on_comment(mock.Mock(), mock.Mock())
+    for educator in educators:
+        assert educator.on_comment.called
+
