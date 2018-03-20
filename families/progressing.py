09d8b7ca4826f0650eaaf0bf5f61ab4655798fdc
Mike Larsson <mikelarssonftw@gmail.com>
2018-03-20 14:52:18 -0400

    send email to family account on feedback (#1710)

--- families/progressing.py
+++ families/progressing.py
@@ -19,3 +19,13 @@ class ProgressOwner(BaseActor):
 
     def on_comment_received(self, progress, comment):
         notify.send(comment.user, recipient=self.user, verb="posted", action_object=comment, target=progress)
+
+        path = reverse('challenges:challenge_progress', kwargs={
+            "challenge_id": progress.challenge.id,
+            "username": progress.owner.username,
+            "stage": Stage(comment.stage).name
+        })
+        send(template_name='family-account-mentor-feedback', to=progress.owner, merge_vars={
+            "username": progress.owner.username,
+            "button_url": url_for_template(path)
+        })
