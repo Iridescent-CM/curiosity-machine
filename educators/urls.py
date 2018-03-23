f4bc4b40661ce50adc4feefd1f18069e5e6fcc41
Mike Larsson <mikelarssonftw@gmail.com>
2018-03-23 11:13:26 -0400

    change coach signup to happen concurrently with profile creation (#1726)

--- educators/urls.py
+++ educators/urls.py
@@ -14,6 +14,5 @@ urlpatterns = [
     url(r'^home/activity/$', activity, name='activity'),
     url(r'^data/impact_survey/$', impact_data, name='update_impact_survey'),
     url(r'^data/progress_posts/$', comments, name='progress_graph_data'),
-    url(r'^coach/$', coach, name='create_coach'),
     url(r'^coach-conversion/$', coach_conversion, name="coach_conversion"),
 ]
