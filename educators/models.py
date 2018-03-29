cbcfea1bcb915fa75a7aef1be63a3ebf8f098b47
Mike Larsson <mikelarssonftw@gmail.com>
2018-03-29 15:36:19 -0400

    add tests for is_coach and coach_full_access

--- educators/models.py
+++ educators/models.py
@@ -15,17 +15,21 @@ class EducatorProfile(BaseProfile):
 
     @cached_property
     def full_coach_access(self):
-       presurvey = get_survey(settings.AICHALLENGE_COACH_PRE_SURVEY_ID)
-       if presurvey.active:
-         response = presurvey.response(self.user)
-         return response.completed
-       return True
+        if not self.is_coach:
+            return False
+
+        presurvey = get_survey(settings.AICHALLENGE_COACH_PRE_SURVEY_ID)
+        if presurvey.active:
+            response = presurvey.response(self.user)
+            return response.completed
+
+        return True
 
     @cached_property
     def is_coach(self):
-       if settings.AICHALLENGE_COACH_MEMBERSHIP_ID:
-         return self.user.membership_set.filter(is_active=True, id=settings.AICHALLENGE_COACH_MEMBERSHIP_ID).exists()
-       return False
+        if settings.AICHALLENGE_COACH_MEMBERSHIP_ID:
+            return self.user.membership_set.filter(is_active=True, id=settings.AICHALLENGE_COACH_MEMBERSHIP_ID).exists()
+        return False
 
 class ImpactSurvey(models.Model):
     student_count = models.PositiveIntegerField(default=0, blank=True)
