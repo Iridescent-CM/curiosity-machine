aaa6b926b9ff2d4cea7a5e1aa7e405670b996691
Mike Larsson <mikelarssonftw@gmail.com>
2018-04-05 15:20:39 -0400

    catch some more smtp exceptions and ignore if DEBUG (#1760)

--- curiositymachine/allauth_adapter.py
+++ curiositymachine/allauth_adapter.py
@@ -3,6 +3,7 @@ from curiositymachine.context_processors.google_analytics import add_event
 from django.conf import settings
 from django.contrib import messages
 from django.core.exceptions import MultipleObjectsReturned
+from smtplib import SMTPException
 import logging
 
 logger = logging.getLogger(__name__)
@@ -21,7 +22,7 @@ class AllAuthAdapter(DefaultAccountAdapter):
     def send_mail(self, template_prefix, email, context):
         try:
             super().send_mail(template_prefix, email, context)
-        except ConnectionRefusedError as e:
+        except (ConnectionRefusedError, SMTPException) as e:
             if settings.DEBUG:
                 # running with DEBUG and no SMTP is okay
                 logger.exception("Exception encountered sending mail, DEBUG on")
