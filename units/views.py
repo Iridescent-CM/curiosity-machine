4c6a37b930d3a23c97c4c0c90a6c7534d3ffde11
Mike Larsson <mikelarssonftw@gmail.com>
2018-04-11 15:57:44 -0400

    #1765 units (#1774)
    * move units page off deprecated layout
    
    * move unit page out of deprecated layout

--- units/views.py
+++ units/views.py
@@ -2,15 +2,15 @@ from .models import Unit
 from django.shortcuts import render, get_object_or_404
 
 def units(request):
-	units = Unit.objects.filter(listed=True).order_by('id')
-	return render(request, 'units/units.html', {'units': units})
+    units = Unit.objects.filter(listed=True).order_by('id')
+    return render(request, 'units/units.html', {'units': units})
 
 def unit(request, unit_id=None, slug=None):
-	unit = get_object_or_404(Unit, id=unit_id) if unit_id else get_object_or_404(Unit, slug=slug)
-	challenges = unit.challenges.all().order_by('unitchallenge__display_order')
-	resources = unit.resources.all()
-	return render(request, 'units/unit.html', {
-		'unit': unit,
-		'challenges': challenges,
-		'resources': resources
-	})
+    unit = get_object_or_404(Unit, id=unit_id) if unit_id else get_object_or_404(Unit, slug=slug)
+    challenges = unit.challenges.all().order_by('unitchallenge__display_order')
+    resources = unit.resources.all()
+    return render(request, 'units/unit.html', {
+        'unit': unit,
+        'challenges': challenges,
+        'resources': resources
+    })
