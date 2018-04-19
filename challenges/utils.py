6e57035d05ba4489a95b6d6eb620291a9547ed3d
Mike Larsson <mikelarssonftw@gmail.com>
2018-04-19 15:10:41 -0400

    redirect to most recent of owners stages

--- challenges/utils.py
+++ challenges/utils.py
@@ -2,8 +2,6 @@ from .models import Stage
 
 def get_stage_for_progress(progress, default=Stage.plan):
     try:
-        # find most recent activity and use its stage
-        return Stage(progress.comments.order_by("-created")[0].stage)
+        return Stage(progress.comments.filter(user_id=progress.owner_id).order_by("-created")[0].stage)
     except IndexError:
-        # if no activity, use default
         return default
