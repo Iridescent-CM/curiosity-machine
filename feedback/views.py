a5415f4279533bcc2cfeff81350593964b7f7e9f
James-Robe <james@iridescentlearning.org>
2018-05-14 11:39:33 -0400

    feedback url includes question id
    the feedback question URL now includes the question ID. #1812

--- feedback/views.py
+++ feedback/views.py
@@ -6,13 +6,14 @@ from django.contrib.auth.decorators import login_required
 from curiositymachine.decorators import mentor_or_current_user
 from challenges.models import Challenge
 from .forms import FeedbackQuestionForm
+from .models import FeedbackQuestion
 
 @require_POST
 @login_required
 @mentor_or_current_user
-def make_feedback_result(request, challenge_id, username):
+def make_feedback_result(request, challenge_id, username, feedback_id):
     challenge = get_object_or_404(Challenge, id=challenge_id)
-    feedback_question = challenge.feedback_question
+    feedback_question = get_object_or_404(FeedbackQuestion, id=feedback_id)
     form = FeedbackQuestionForm(request.POST, model=feedback_question)
     if form.is_valid():
         feedback_result = form.get_feedback_result(request.user, challenge=challenge)
