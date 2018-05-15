9af477a8cb252b768970cb6c541ad0d6f313e821
Mike Larsson <mikelarssonftw@gmail.com>
2018-05-15 15:05:06 -0400

    admin improvements

--- feedback/admin.py
+++ feedback/admin.py
@@ -11,10 +11,13 @@ class FeedbackResultAdmin(admin.ModelAdmin):
     class Meta:
         model = FeedbackResult
 
-    list_display = ['id', 'feedback_question', 'challenge_name', 'user']
+    list_display = ['id', 'question', 'answer', 'challenge_name', 'user']
 
     def challenge_name(self, obj):
         return obj.challenge.name
+
+    def question(self, obj):
+        return obj.feedback_question.question
 
 admin.site.register(FeedbackQuestion, FeedbackQuestionAdmin)
 admin.site.register(FeedbackResult, FeedbackResultAdmin)
