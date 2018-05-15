9b07c4ca0aed868037308790cfb50004de89c222
Mike Larsson <mikelarssonftw@gmail.com>
2018-05-15 17:11:07 -0400

    fix feedback url again

--- feedback/urls.py
+++ feedback/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^(?P<challenge_id>\d+)/(?P<username>[\w.@+-]+)/feedback/(?P<feedback_id>\d+)/', make_feedback_result, name='submit'),
+    url(r'^(?P<feedback_id>\d+)/', make_feedback_result, name='submit'),
 ]
