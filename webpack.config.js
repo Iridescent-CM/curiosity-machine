a1fa106187237beb655a34f4535e0dbfb63207b8
Mike Larsson <mikelarssonftw@gmail.com>
2018-06-26 11:14:36 -0400

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,16 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
