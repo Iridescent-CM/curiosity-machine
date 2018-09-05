fa69683bfea3d1a927ff1e8d510332b0ae6873f0
Mike Larsson <mikelarssonftw@gmail.com>
2018-09-05 16:17:26 -0400

    add name fields to student form

--- students/forms.py
+++ students/forms.py
@@ -31,12 +31,30 @@ class StudentProfileEditForm(ProfileModelForm):
         required=False
     )
 
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+
     def save_related(self, obj):
         if self.cleaned_data.get("image_url"):
             img = Image.from_source_with_job(self.cleaned_data['image_url']['url'])
             obj.image = img
 
         return obj
+
+    def update_user(self):
+        if self.cleaned_data.get('first_name'):
+            self.user.first_name = self.cleaned_data['first_name']
+        if self.cleaned_data.get('last_name'):
+            self.user.last_name = self.cleaned_data['last_name']
+
+    def get_initial(self, user, instance, **kwargs):
+        return super().get_initial(
+            user,
+            instance,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            **kwargs
+        )
 
     def get_role(self):
         return UserRole.student
