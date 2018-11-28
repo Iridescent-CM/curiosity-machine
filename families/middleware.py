cfb22a369d404adad5691e74777d9be4505b2034
Mike Larsson <mikelarssonftw@gmail.com>
2018-11-28 14:37:01 -0500

    show permission slip screen before other requirements screen

--- families/middleware.py
+++ families/middleware.py
@@ -4,22 +4,26 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 from surveys import get_survey
 from .aichallenge import Stage
-from .views import prereq_interruption, postsurvey_interruption
+from .views import prereq_interruption, postsurvey_interruption, sign_slip
 
 class SignUpPrerequisitesMiddleware(MiddlewareMixin):
     """
     Middleware that checks for pre-survey and permission slips.
     """
     def process_view(self, request, view, view_args, view_kwargs):
+        user = request.user
         if (
-            request.user.is_authenticated()
-            and request.user.extra.is_family
-            and not request.user.familyprofile.full_access
+            user.is_authenticated()
+            and user.extra.is_family
             and not (
                 whitelisted(view, 'public', 'maybe_public', 'unapproved_family')
                 or whitelist_regex.match(request.path.lstrip('/'))
             )
+            and not user.familyprofile.full_access
         ):
+            if not user.familyprofile.permission_slip_signed:
+                return sign_slip(request)
+
             return prereq_interruption(request)
 
 class PostSurveyMiddleware(MiddlewareMixin):
--- families/models.py
+++ families/models.py
@@ -32,19 +32,30 @@ class FamilyProfile(BaseProfile):
         return self.check_full_access()
 
     def check_full_access(self):
+        return self.presurvey_completed and self.permission_slip_signed
+
+    @cached_property
+    def presurvey_completed(self):
         presurvey = get_survey(settings.AICHALLENGE_FAMILY_PRE_SURVEY_ID)
         if presurvey.active:
             response = presurvey.response(self.user)
             if not response.completed:
                 return False
 
-        consent = FamilyConsentTemplate(settings.AICHALLENGE_FAMILY_CONSENT_TEMPLATE_ID)
-        if consent.active:
-            signature = consent.signature(self.user)
-            if not signature.signed:
-                return False
-
         return True
+
+    @cached_property
+    def permission_slip_signed(self):
+        return PermissionSlip.objects.filter(account=self.user).exists()
+        # TO DO: migrate existing signatures to slips
+
+        #consent = FamilyConsentTemplate(settings.AICHALLENGE_FAMILY_CONSENT_TEMPLATE_ID)
+        #if consent.active:
+        #    signature = consent.signature(self.user)
+        #    if not signature.signed:
+        #        return False
+        #
+        #return True
 
     def check_welcome(self):
         if self.check_full_access() and not self.welcomed:
