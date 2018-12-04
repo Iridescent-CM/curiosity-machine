13fe6bd9e391f6698ddcde05bd0fdd58a7e0d421
Mike Larsson <mikelarssonftw@gmail.com>
2018-12-04 14:18:25 -0500

    add survey filter by survey id

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from enumfields.admin import EnumFieldListFilter
+from . import get_survey
 from .models import *
 from .updating import Updating
 
+class SurveyNameFilter(admin.SimpleListFilter):
+    title = "Name"
+    parameter_name = 'name'
+
+    def lookups(self, request, model_admin):
+        ids = SurveyResponse.objects.distinct('survey_id').values_list('survey_id', flat=True)
+        return ((id, "%s - %s" % (id, getattr(get_survey(id), "name", "n/a"))) for id in ids)
+
+    def queryset(self, request, queryset):
+        return queryset.filter(survey_id=self.value())
+
 class SurveyResponseAdmin(admin.ModelAdmin):
     list_display = ['id', 'survey_id', 'user', 'status', 'created_at', 'updated_at']
-    list_filter = [('status', EnumFieldListFilter)]
+    list_filter = [('status', EnumFieldListFilter), SurveyNameFilter]
     readonly_fields = ['id', 'survey_id', 'user', 'created_at', 'updated_at']
     search_fields = ['id', 'survey_id', 'user__username']
     ordering = ('-created_at',)
