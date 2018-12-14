345372482b5c724d252f0706950f0e36983b61ce
Mike Larsson <mikelarssonftw@gmail.com>
2018-12-14 14:40:33 -0500

    remove old hellosign code we don't need (#2125)

--- hellosign/__init__.py
+++ hellosign/__init__.py
@@ -1,9 +1,1 @@
-from .signrequest import send_student_consent_form as _send_student_consent_form
-import django_rq
-
-
 default_app_config = 'hellosign.apps.HellosignConfig'
-
-
-def send_student_consent_form(sender):
-    django_rq.enqueue(_send_student_consent_form, sender)
