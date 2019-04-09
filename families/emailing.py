4598d55e6e978d3a69b2fd6839331fe42aef7aff
Rachel Wyatt <wyattr@gmail.com>
2019-04-08 20:37:15 -0400

    Revert "Revert "recovered emailing.py but dropped unnecessary method""
    This reverts commit e7739b265df8e892d961894679453e0bc13a4b12.

---
+++ families/emailing.py
@@ -0,0 +1,5 @@
+from cmemails import send
+
+class Emailing:
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
--- families/tests/test_progressing.py
+++ families/tests/test_progressing.py
@@ -1,23 +1,22 @@
-# from challenges.factories import *
-# from challenges.progressing import Progressing
-# from cmcomments.factories import *
-# from educators.factories import *
-# from memberships.factories import *
-# from ..aichallenge import *
-# from ..factories import *
-# from ..progressing import *
-# import mock
-# import pytest
+from challenges.factories import *
+from challenges.progressing import Progressing
+from cmcomments.factories import *
+from educators.factories import *
+from memberships.factories import *
+from ..factories import *
+from ..progressing import *
+import mock
+import pytest
 
-# @pytest.mark.django_db
-# def test_family_emailed_when_coach_posts():
-#     coach = EducatorFactory()
-#     family = FamilyFactory()
-#     progress = ProgressFactory(owner=family)
-#     MembershipFactory(challenges=[progress.challenge], members=[coach, family])
-#     comment = CommentFactory(challenge_progress=progress, user=coach)
-#     with mock.patch("families.progressing.send") as send:
-#         Progressing(progress=progress).on_comment(comment)
-#         assert send.called
-#         assert send.call_args[1]['template_name'] == 'family-account-mentor-feedback'
+@pytest.mark.django_db
+def test_family_emailed_when_coach_posts():
+    coach = EducatorFactory()
+    family = FamilyFactory()
+    progress = ProgressFactory(owner=family)
+    MembershipFactory(challenges=[progress.challenge], members=[coach, family])
+    comment = CommentFactory(challenge_progress=progress, user=coach)
+    with mock.patch("families.progressing.send") as send:
+        Progressing(progress=progress).on_comment(comment)
+        assert send.called
+        assert send.call_args[1]['template_name'] == 'family-account-mentor-feedback'
 
