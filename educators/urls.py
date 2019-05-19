e6947f74ffcfbbdf5800f9415f22d315ca3d4ee5
Rachel Wyatt <wyattr@gmail.com>
2019-05-19 16:15:28 -0400

    update layout of participants list page

--- educators/urls.py
+++ educators/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     url(r'^profile/new/$', create, name="create_profile"),
     url(r'^profile/edit/$', edit, name="edit_profile"),
-    url(r'^home/$', challenges, name="home"),
+    url(r'^home/$', aifc, name="home"),
     url(r'^home/challenges/(?P<challenge_id>\d+)/$', challenge, name='challenge'),
     url(r'^home/students/$', students, name='students'),
     url(r'^home/students/(?P<student_id>\d+)/$', student, name='student'),
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^home/students/(?P<student_id>\d+)/challenge/(?P<challenge_id>\d+)/$', conversation, name='conversation'),
     url(r'^home/guides/$', guides, name='guides'),
     url(r'^home/activity/$', activity, name='activity'),
-    url(r'^home/aifc/$', aifc, name='aifc'),
+    url(r'^home/challenges/$', challenges, name='challenges'),
     url(r'^data/impact_survey/$', impact_data, name='update_impact_survey'),
     url(r'^data/progress_posts/$', comments, name='progress_graph_data'),
     url(r'^coach-conversion/$', coach_conversion, name="coach_conversion"),
