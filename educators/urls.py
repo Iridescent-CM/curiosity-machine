8e2faaa5f7eaa4468242cc0955a997154a772625
Rachel Wyatt <wyattr@gmail.com>
2019-06-11 10:30:03 -0400

    get rid of all coach converstion code

--- educators/urls.py
+++ educators/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     url(r'^home/challenges/$', challenges, name='challenges'),
     url(r'^data/impact_survey/$', impact_data, name='update_impact_survey'),
     url(r'^data/progress_posts/$', comments, name='progress_graph_data'),
-    url(r'^coach-conversion/$', coach_conversion, name="coach_conversion"),
     url(r'^coach-removal/$', coach_removal, name="coach_removal"),
 ]
 
