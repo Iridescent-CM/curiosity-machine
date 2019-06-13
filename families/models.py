d06fc8d02b1798a6b6210b18eda17c05d4ce6e82
Mike Larsson <mikelarssonftw@gmail.com>
2019-06-13 11:03:41 -0400

    remove phone number from family form

--- families/models.py
+++ families/models.py
@@ -7,7 +7,6 @@ from django.utils.timezone import now
 from enum import Enum
 from images.models import Image
 from locations.models import Location
-from phonenumber_field.modelfields import PhoneNumberField
 from profiles.models import BaseProfile
 from surveys import get_survey
 
@@ -21,7 +20,6 @@ __all__ = [
 
 class FamilyProfile(BaseProfile):
     image = models.ForeignKey(Image, null=True, blank=True, on_delete=models.SET_NULL)
-    phone = PhoneNumberField(null=True, blank=True)
     location = models.ForeignKey(Location, null=False, blank=False, on_delete=models.PROTECT)
     welcomed = models.DateTimeField(null=True, blank=True)
     members_confirmed = models.BooleanField(default=False)
