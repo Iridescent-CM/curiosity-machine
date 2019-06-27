13920b38c1deddb4b51be01496be8ca776251c7b
Rachel Wyatt <wyattr@gmail.com>
2019-06-27 18:24:02 -0400

    url

--- lessons/urls.py
+++ lessons/urls.py
@@ -12,10 +12,7 @@ router.register(r'comment', CommentViewSet, base_name="comment")
 router.register(r'quiz', QuizViewSet, base_name="quiz")
 router.register(r'quiz_result', QuizResultViewSet, base_name="quiz-result")
 urlpatterns = router.urls
-
-# rest_framework doesn't have a good affordance for decorators, so
-# for now we're replacing the LessonViewSet list route with a whitelisted version
-# this way (FIXME)
+# using a url pattern to allow for use of whitelist decorator since it takes a view rather than viewset
 urlpatterns += [
     url(r'^lessons', public(LessonViewSet.as_view({'get':'list'})), name='lesson-list')
 ]
