9397c2f53b18490dd1cab0cd94eea72e0a036f59
Mike Larsson <mikelarssonftw@gmail.com>
2019-06-28 10:29:00 -0400

    the route we want used has to be listed first

--- lessons/urls.py
+++ lessons/urls.py
@@ -5,17 +5,17 @@ from .views import *
 
 public = whitelist('public')
 
+# rest_framework doesn't have a good affordance for decorators, so
+# for now we're routing the LessonViewSet list route with a whitelisted version
+# this way, which should eclipse the route the SimpleRouter sets up below (FIXME)
+urlpatterns = [
+    url(r'^lesson/$', public(LessonViewSet.as_view({'get':'list'})), name='lesson-list')
+]
+
 router = SimpleRouter()
 router.register(r'lesson', LessonViewSet, base_name="lesson")
 router.register(r'lesson_progress', LessonProgressViewSet, base_name="lesson-progress")
 router.register(r'comment', CommentViewSet, base_name="comment")
 router.register(r'quiz', QuizViewSet, base_name="quiz")
 router.register(r'quiz_result', QuizResultViewSet, base_name="quiz-result")
-urlpatterns = router.urls
-
-# rest_framework doesn't have a good affordance for decorators, so
-# for now we're replacing the LessonViewSet list route with a whitelisted version
-# this way (FIXME)
-urlpatterns += [
-    url(r'^lesson/$', public(LessonViewSet.as_view({'get':'list'})), name='lesson-list')
-]
+urlpatterns += router.urls
