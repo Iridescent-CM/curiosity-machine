fb90f3e4308118a9eab6d85a4f7dc98a02b416c1
Mike Larsson <mikelarssonftw@gmail.com>
2020-05-04 14:11:16 -0400

    fix typo in season marker class

--- season_markers/models.py
+++ season_markers/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 class SeasonMarker(models.Model):
+
+    class Meta:
+        unique_together = ("start", "end", "name")
+
     start = models.DateTimeField()
     end = models.DateTimeField()
     name = models.CharField(max_length=128)
@@ -11,20 +15,17 @@ class SeasonMarker(models.Model):
         related_name="season_markers"
     )
 
-    class meta:
-        unique_together = ("start", "end", "name")
-
     def __str__(self):
         return "SeasonMarker: id={}, name={}".format(self.id, self.name)
 
 class SeasonParticipation(models.Model):
+
+    class Meta:
+        unique_together = ("user", "season_marker")
+
     user = models.ForeignKey(get_user_model(), null=False, blank=False)
     season_marker = models.ForeignKey(SeasonMarker, null=False, blank=False)
 
     joined_season_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        unique_together = ("user", "season_marker")
-
