e86b878d85584e975749af189e4d7feb2225e275
Mike Larsson <mikelarssonftw@gmail.com>
2020-06-04 16:51:08 -0400

    update usage of include for 2.2

--- cmcomments/urls.py
+++ cmcomments/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "comments"
+
 urlpatterns = [
     url(r'^$', views.comments, name='comments')
 ]
--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,8 @@ from .views import *
 
 from curiositymachine.decorators import whitelist
 
+app_name = "profiles"
+
 urlpatterns = [
     url(r'^profiles/$', choose_profile, name="profiles"),
     url(r'^profile/edit/$', whitelist('unapproved_family')(edit_profile), name="edit_profile"),
